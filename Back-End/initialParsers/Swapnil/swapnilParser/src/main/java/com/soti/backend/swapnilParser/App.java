abae793411056dc83e368b781116831e53055ee7
Swapnil Shah <swapnil.shah@soti.net>
2016-06-06 10:29:25 -0400

    pushing some changes in back-end java files

--- Back-End/initialParsers/Swapnil/swapnilParser/src/main/java/com/soti/backend/swapnilParser/App.java
+++ Back-End/initialParsers/Swapnil/swapnilParser/src/main/java/com/soti/backend/swapnilParser/App.java
@@ -1,6 +1,16 @@
 package com.soti.backend.swapnilParser;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.ISODateTimeFormat;
+import org.ocpsoft.prettytime.PrettyTime;
+
 import com.mdimension.jchronic.Chronic;
 import com.mdimension.jchronic.utils.Span;
 import com.soti.backend.swapnilParser.PdfToText;
@@ -16,7 +26,18 @@ public class App
        pdfManager.setFilePath(System.getProperty("user.dir") + "/src/main/java/com/soti/backend/swapnilParser/Syllabus.pdf");
        String rawtext= pdfManager.ToText();
        System.out.println(rawtext);
-       Span actualSpan = Chronic.parse(rawtext);
-       System.out.println(actualSpan);
+       SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd"); 
+       
+       Date t = new Date(); 
+       //This is the date which has the format of ft defined above
+       
+       try { 
+           //parse the date based on the format ft defined above
+           t = ft.parse("2016-09-12"); 
+           System.out.println("Parsed date : " + t); 
+       } catch (ParseException e) { 
+           System.out.println("Not able to parse " + ft); 
+       }
+       
 }
 }
