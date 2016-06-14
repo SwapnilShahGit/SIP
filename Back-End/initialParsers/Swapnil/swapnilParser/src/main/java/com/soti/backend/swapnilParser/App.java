2418b43b385060941b000d632427489e9f863e19
Swapnil Shah <swapnil.shah@soti.net>
2016-06-14 16:11:11 -0400

    adding updated code

--- Back-End/initialParsers/Swapnil/swapnilParser/src/main/java/com/soti/backend/swapnilParser/App.java
+++ Back-End/initialParsers/Swapnil/swapnilParser/src/main/java/com/soti/backend/swapnilParser/App.java
@@ -1,23 +1,14 @@
 package com.soti.backend.swapnilParser;
 
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Formatter;
-import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 import org.joda.time.format.DateTimeFormatterBuilder;
 import org.joda.time.format.DateTimeParser;
-import org.joda.time.format.ISODateTimeFormat;
-import org.ocpsoft.prettytime.PrettyTime;
-
-import com.mdimension.jchronic.Chronic;
-import com.mdimension.jchronic.utils.Span;
-import com.soti.backend.swapnilParser.PdfToText;
 
 /**
  * Hello world!
@@ -29,9 +20,19 @@ public class App
        PdfToText pdfManager = new PdfToText();
        pdfManager.setFilePath(System.getProperty("user.dir") + "/src/main/java/com/soti/backend/swapnilParser/Syllabus.pdf");
        String rawtext = pdfManager.ToText();
-       String pattern = createRegex("^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]|(?:Jan|Mar|May|Jul|Aug|Oct|Dec)))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2]|(?:Jan|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec))\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)(?:0?2|(?:Feb))\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9]|(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep))|(?:1[0-2]|(?:Oct|Nov|Dec)))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$");
+       String pattern = createRegex("^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]|(?:Jan|Mar|May|Jul|Aug|Oct|Dec)))\\1|(?:(?:29|30)(\\/|-|\\.)(?:0?[1,3-9]|1[0-2]|(?:Jan|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec))\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)(?:0?2|(?:Feb))\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9]|(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep))|(?:1[0-2]|(?:Oct|Nov|Dec)))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$");
        
-       System.out.println(rawtext);
+       DateTime myDate;
+       if (DateTime.parse("01/1/2000", "dd-MM-yyyy")){
+    	   System.out.println("found 1");
+       }
+       Pattern r = Pattern.compile(pattern);
+       Matcher m = r.matcher("01/1/2000");
+       if (m.find()){
+    	   System.out.println("found 1");
+       } else {
+    	   System.out.println("fdoajshfahfsaihdf");
+       }
        DateTimeParser[] parsers = { 
     		   DateTimeFormat.forPattern("yyyy-MM-dd").getParser(),
     		   DateTimeFormat.forPattern("yyyy/MM/dd").getParser()
@@ -39,7 +40,7 @@ public class App
        
        DateTimeFormatter formatter = new DateTimeFormatterBuilder().append(null, parsers).toFormatter();
        DateTime date1 = formatter.parseDateTime("2016/03/02");
-       DateTime date2 = formatter.;
+       //DateTime date2 = formatter;
 
        System.out.println(date1);
        
