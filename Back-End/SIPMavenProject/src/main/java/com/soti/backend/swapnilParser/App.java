69a2d7bd653bbd0a22595b7cbc68c53343d19d5b
Swapnil Shah <swapnil.shah@mail.utoronto.ca>
2016-06-21 10:35:28 -0400

    adding code from back-end

--- Back-End/SIPMavenProject/src/main/java/com/soti/backend/swapnilParser/App.java
+++ Back-End/SIPMavenProject/src/main/java/com/soti/backend/swapnilParser/App.java
@@ -3,9 +3,12 @@ package com.soti.backend.swapnilParser;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Formatter;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.joda.time.DateTime;
 import org.joda.time.LocalDate;
@@ -23,53 +26,75 @@ import com.mdimension.jchronic.Chronic;
 import com.mdimension.jchronic.utils.Span;
 import com.soti.backend.swapnilParser.PdfToText;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main(String[] args) throws IOException {
-       PdfToText pdfManager = new PdfToText();
+       ////Pranali's section
+    	PdfToText pdfManager = new PdfToText();
        pdfManager.setFilePath(System.getProperty("user.dir") + "/src/main/java/com/soti/backend/swapnilParser/Syllabus.pdf");
        String rawtext = pdfManager.ToText();
-       DateTimeParser[] parsers = { 
-    		   DateTimeFormat.forPattern("yyyy-MM-dd").getParser(),
-    		   DateTimeFormat.forPattern("yyyy/MM/dd").getParser(),
-    		   DateTimeFormat.forPattern("MM/dd/yyyy").getParser(),
-    		   DateTimeFormat.forPattern("MM-dd-yyyy").getParser(),
-    		   DateTimeFormat.forPattern("MMddyyyy").getParser(),
-    		   DateTimeFormat.forPattern("yyyyMMdd").getParser(),
-
-       };
+       ////Darren's section
+       String lines[] = rawtext.split("\\r?\\n");
+       System.out.println(lines.length);
+       String pattern = "(?<!\\S)(?:(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]|(?:Jan|Mar|May|Jul|Aug|Oct|Dec)))\\1|"
+       		+ "(?:(?:29|30)(\\/|-|\\.)(?:0?[1,3-9]|1[0-2]|(?:Jan|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec))\\2))"
+       		+ "(?:(?:1[6-9]|[2-9]\\d)?\\d{2})|(?:(?:1[6-9]|[2-9]\\d)?\\d{2})(\\/|-|\\.)(?:(?:(?:0?[13578]|1[02]|"
+       		+ "(?:Jan|Mar|May|Jul|Aug|Oct|Dec))\\3(?:31))|(?:(?:0?[1,3-9]|1[0-2]|(?:Jan|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)"
+       		+ ")\\3(?:29|30)))|(?:29(\\/|-|\\.)(?:0?2|(?:Feb))\\4(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|"
+       		+ "(?:(?:16|[2468][048]|[3579][26])00))))|(?:(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|"
+       		+ "[2468][048]|[3579][26])00)))(\\/|-|\\.)(?:0?2|(?:Feb))\\5(?:29))|(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9]|"
+       		+ "(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep))|(?:1[0-2]|(?:Oct|Nov|Dec)))\\6(?:(?:1[6-9]|[2-9]\\d)?\\d{2})|(?:(?:1[6-9]|"
+       		+ "[2-9]\\d)?\\d{2})(\\/|-|\\.)(?:(?:0?[1-9]|(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep))|(?:1[0-2]|(?:Oct|Nov|Dec)))\\7"
+       		+ "(?:0?[1-9]|1\\d|2[0-8]))(?!\\S)";
+       Pattern r = Pattern.compile(pattern);
+       List<String> matchedcharacters = new ArrayList<String>();
        
+       for (String x : lines){
+    	   Matcher m = r.matcher(x);
+           int count = 0;
+    	   while (m.find()){
+    		   count++;
+    		   System.out.println("Match number " + count);
+    		   System.out.println("start(): " + m.start());
+    		   System.out.println("end(): " + m.end());
+    		   matchedcharacters.add(x);
+    		   System.out.println(x);
+    	   }   
+       }
+       
+       
+
+       ////Pranali check here to make sure all dates are valid
+       //what do we do if the date is not valid?
+       
+       ////Swapnil's section
+       //properly parse the date into the correct format
+       //
+       List<DateTime> outputdates = new ArrayList<DateTime>();
        Parser parser = new Parser();
-       List<DateGroup> group = parser.parse("two weeks after September 4 2012");
-       List<Date> date = (group.get(0)).getDates();
-       Date sampledate = date.get(0);
-       DateTime dt = new DateTime(sampledate);
-       System.out.println(dt.getMonthOfYear() + "-" + dt.getDayOfMonth() + "-" + dt.getYear());
+       for (String x : matchedcharacters){
+    	   List<DateGroup> group = parser.parse(x);
+    	   if (group.toString() == "[]"){
+    		   continue;
+    	   }
+    	   List<Date> date = (group.get(0)).getDates();
+           Date sampledate = date.get(0);
+           DateTime dt = new DateTime(sampledate);
+           outputdates.add(dt);
+           //System.out.println(dt.getMonthOfYear() + "-" + dt.getDayOfMonth() + "-" + dt.getYear());
+       }
+       
+       for (DateTime dt : outputdates){
+           System.out.println(dt.getMonthOfYear() + "-" + dt.getDayOfMonth() + "-" + dt.getYear());
+           System.out.println(dt.getHourOfDay());
+       }
+       
 
-       DateTimeFormatter formatter = new DateTimeFormatterBuilder().append(null, parsers).toFormatter();
-       //LocalDate date1 = formatter.parseLocalDate("Mon May 4");
-       //System.out.println(date1.toDate());
+
        
        
 }
  
 }
 
-//SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd"); 
- 
-/*
-Date t = new Date(); 
-//This is the date which has the format of ft defined above
-
-try { 
-    //parse the date based on the format ft defined above
-    t = ft.parse("2016-09-12"); 
-    System.out.println("Parsed date : " + t); 
-} catch (ParseException e) { 
-    System.out.println("Not able to parse " + ft); 
-}
-*/
