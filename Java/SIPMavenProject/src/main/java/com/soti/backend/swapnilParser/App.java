a44c23adc4ee119d484baffea9996c9c7bd9349a
Swapnil Shah <swapnil.shah@mail.utoronto.ca>
2016-07-06 15:18:51 -0400

    adding JSON files

--- Java/SIPMavenProject/src/main/java/com/soti/backend/swapnilParser/App.java
+++ Java/SIPMavenProject/src/main/java/com/soti/backend/swapnilParser/App.java
@@ -44,13 +44,57 @@ public class App
 			 pdfManager.setFilePath(listOfFiles[i].toString());
 			 String rawtext = pdfManager.ToText();
 			 obj.put("rawtext", rawtext);
+		     String lines[] = rawtext.split("\\r?\\n");
+		     String pattern = "(?<!\\S)(?:(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]|(?:Jan|Mar|May|Jul|Aug|Oct|Dec)))\\1|"
+		        		+ "(?:(?:29|30)(\\/|-|\\.)(?:0?[1,3-9]|1[0-2]|(?:Jan|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec))\\2))"
+		        		+ "(?:(?:1[6-9]|[2-9]\\d)?\\d{2})|(?:(?:1[6-9]|[2-9]\\d)?\\d{2})(\\/|-|\\.)(?:(?:(?:0?[13578]|1[02]|"
+		        		+ "(?:Jan|Mar|May|Jul|Aug|Oct|Dec))\\3(?:31))|(?:(?:0?[1,3-9]|1[0-2]|(?:Jan|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)"
+		        		+ ")\\3(?:29|30)))|(?:29(\\/|-|\\.)(?:0?2|(?:Feb))\\4(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|"
+		        		+ "(?:(?:16|[2468][048]|[3579][26])00))))|(?:(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|"
+		        		+ "[2468][048]|[3579][26])00)))(\\/|-|\\.)(?:0?2|(?:Feb))\\5(?:29))|(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9]|"
+		        		+ "(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep))|(?:1[0-2]|(?:Oct|Nov|Dec)))\\6(?:(?:1[6-9]|[2-9]\\d)?\\d{2})|(?:(?:1[6-9]|"
+		        		+ "[2-9]\\d)?\\d{2})(\\/|-|\\.)(?:(?:0?[1-9]|(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep))|(?:1[0-2]|(?:Oct|Nov|Dec)))\\7"
+		        		+ "(?:0?[1-9]|1\\d|2[0-8]))(?!\\S)";
+		        Pattern r = Pattern.compile(pattern);
+		        List<String> matchedcharacters = new ArrayList<String>();
+
+		        int count = 0;
+		        for (String x : lines){
+		     	   Matcher m = r.matcher(x);
+		     	   while (m.find()){
+		     		   count++;
+		     		   matchedcharacters.add(x);
+		     	   }   
+		        }
+		     
+		     obj.put("university", "");
+		     obj.put("code", "");
+		     obj.put("name", "");
+		     obj.put("description", "");
+		     obj.put("division", "");
+		     obj.put("professorname", "");
+		     obj.put("professoremail", "");
+		     obj.put("professorwebsite", "");
+		     obj.put("officelocation", "");
+		     obj.put("officehourstimes", "");
+		     obj.put("semester", "");
+		     obj.put("gradedevaluations", "");
+		     obj.put("coursecode", "");
+		     obj.put("coursecode", "");
+
+
+		        
+		        
+		        
 			 try{
-				 FileWriter file = new FileWriter(System.getProperty("user.dir") + "/PDFS/JSONOutput" + );
-				 file.write(obj.);
+				 FileWriter file = new FileWriter(System.getProperty("user.dir") + "/PDFS/JSONOutput/testfile.json");
+				 file.write(obj.toJSONString());
+				 file.flush();
+				 file.close();
 						 
 						 	
 			 } catch (IOException e){
-				 
+				 e.printStackTrace();
 			 }
 		 }
 
