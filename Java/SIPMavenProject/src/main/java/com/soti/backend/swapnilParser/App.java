88c90a904d3d6a992d79310e62d0afc6fd10c4a4
Swapnil Shah <swapnil.shah@soti.net>
2016-07-22 14:58:54 -0400

    Update App.java

--- Java/SIPMavenProject/src/main/java/com/soti/backend/swapnilParser/App.java
+++ Java/SIPMavenProject/src/main/java/com/soti/backend/swapnilParser/App.java
@@ -3,15 +3,20 @@ package com.soti.backend.swapnilParser;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Formatter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -32,10 +37,11 @@ import com.mdimension.jchronic.utils.Span;
 import com.soti.backend.swapnilParser.PdfToText;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 
 
-public class App 
+public class SyllabusParser 
 {
 	 public static void main(String[] args) throws IOException {
 		 File folder = new File(System.getProperty("user.dir")+ "/PDFS/UnParsedFiles");
@@ -71,13 +77,13 @@ public class App
 		        }
 		    
 		     //course code
-		     String coursecodepattern = "[A-Z]{3}[0-9]{3}[H|Y][1|5][F|S|Y]?";
+		     String coursecodepattern = "[A-Z]{3}\\s?[0-9]{3}[H|Y][1|5][F|S|Y]?";
 		     Pattern coursecode = Pattern.compile(coursecodepattern);
 		     Map<String, Integer> results = new HashMap<String, Integer>();
 		     
 		     count = 0;
 		     Matcher m = coursecode.matcher(rawtext);
-		     String finalcoursecode = "hi";
+		     String finalcoursecode = "";
 		     int maxcoursecode = 0;
 		     while (m.find()){
 		     	count++;
@@ -89,40 +95,104 @@ public class App
 		     		}
 		     	} else{
 		     		results.put(m.group(), 1);
+		     		finalcoursecode = m.group();
 		     	}
-		     	   }   
-		     System.out.println(finalcoursecode);
+		     	   }
+		     if (finalcoursecode.length() == 8){
+		    	 finalcoursecode += "Y";
+		     }
+		     finalcoursecode = finalcoursecode.replaceAll("\\s+","");
 		     obj.put("code", finalcoursecode);
 		     
 		     //name of the university
 		     String universitycampus = "";
-		     //System.out.println(Character.toString(finalcoursecode.charAt(7)));
 		     if (Character.toString(finalcoursecode.charAt(7)).equals("5")){
 		    	 universitycampus = "UTM";
-		    	 //System.out.println(universitycampus);
 		     } else {
 		    	 universitycampus = "UTSG";
 		     }
 		     obj.put("university", universitycampus);
+		     String session;
+		     String optionalcharacter = "";
+		     
+		     if (rawtext.contains("Summer") | rawtext.contains("summer")){
+		    	 session = "5";
+		    	 if (finalcoursecode.endsWith("F") | finalcoursecode.endsWith("S")){
+		    		 optionalcharacter = finalcoursecode.substring(8);
+		    	 }
+		     }else if (rawtext.contains("Winter")| rawtext.contains("winter")) {
+		    	 session = "1";
+		     } else if (rawtext.contains("Winter")| rawtext.contains("winter")){
+		    	 session = "9";
+		     } else {
+		    	 int month = Calendar.getInstance().get(Calendar.MONTH);
+		    	 if (month < 4){
+		    		 session = "1";
+		    	 } else if (month >= 4 && month < 8){
+		    		 session = "5";
+		    	 } else {
+		    		 session = "9";
+		    	 }
+		     }
+		     int year = Calendar.getInstance().get(Calendar.YEAR);
+		     String currentyear;
+		     if (rawtext.contains(Integer.toString(year))){
+		    	 currentyear = Integer.toString(year);
+		     }else {
+		    	 currentyear = Integer.toString(year +1);
+		     }
+		     String requestURL = "";
+		     URLConnection connection;
+		     URL uoftRequest;
+		     Scanner scanner;
+		     try{
+		    	 requestURL = "http://localhost:4242/1.0/courses/" + finalcoursecode + currentyear + session + optionalcharacter;
+		    	 uoftRequest = new URL(requestURL);
+			     connection = uoftRequest.openConnection();
+			     scanner = new Scanner(uoftRequest.openStream());
+		     } catch (Exception e){
+		    	 year += 1;
+		         currentyear = Integer.toString(year);			 
+		    	 requestURL = "http://localhost:4242/1.0/courses/" + finalcoursecode + currentyear + session + optionalcharacter;
+		    	 uoftRequest = new URL(requestURL);
+			     connection = uoftRequest.openConnection();  
+			     scanner = new Scanner(uoftRequest.openStream());
+		     }
+			connection.setDoOutput(true);  
+		     String response = scanner.useDelimiter("\\Z").next();
+		     JSONParser parser2 = new JSONParser();
+		     JSONObject json = null;
+			try {
+				//object = parser2.parse(response);
+				json = (JSONObject)parser2.parse(response);
+
+			} catch (org.json.simple.parser.ParseException e1) {
+				e1.printStackTrace();
+			}
+		     scanner.close();
+		    
+		     
+		     
+		     
 		     
 		     //id of the course
-		     obj.put("id", "");
+		     obj.put("id",json.get("id").toString());
 
 		     //name of the course
-		     obj.put("name", "");
+		     obj.put("name", json.get("name").toString());
 		     //description of the course
-		     obj.put("description", "");
+		     obj.put("description", json.get("description").toString());
 		     //division of the course
-		     obj.put("division", "");
+		     obj.put("division", json.get("division").toString());
 		     //department the course is from
-		     obj.put("department", "");
+		     obj.put("department", json.get("department").toString());
 		     //prerequisities of the course
-		     obj.put("prerequisites", "");
-		     obj.put("exclusions", "");
-		     obj.put("level", "");
-		     obj.put("campus", "");
-		     obj.put("term", "");
-		     obj.put("meeting_sections", "");
+		     obj.put("prerequisites", json.get("prerequisites").toString());
+		     obj.put("exclusions", json.get("exclusions").toString());
+		     obj.put("level", json.get("level").toString());
+		     obj.put("campus", json.get("campus").toString());
+		     obj.put("term", json.get("term").toString());
+		     obj.put("meeting_sections", json.get("meeting_sections").toString());
 		     
 		     //list of graded evaluations
 		     List<String> assignmentlines = new ArrayList<String>();
@@ -136,7 +206,6 @@ public class App
 		     		   assignmentlines.add(x);
 		     	   }
 		        }
-		     System.out.println(assignmentlines.size());
 		        Parser parser = new Parser();
 		        List<DateTime> outputdates = new ArrayList<DateTime>();
 		        int counter = 0;
@@ -154,12 +223,7 @@ public class App
 		             minilist.add(dt);
 		             minilist.add(y);
 		             markedlist.add(minilist);
-		             System.out.println(dt);
-		             System.out.println(y);
-		        }
-		        
-		        for (DateTime dt : outputdates){
-		            System.out.println(dt.getMonthOfYear() + "-" + dt.getDayOfMonth() + "-" + dt.getYear());
+
 		        }
 		       
 		        
@@ -170,7 +234,7 @@ public class App
 		        
 		        
 			 try{
-				 FileWriter file = new FileWriter(System.getProperty("user.dir") + "/PDFS/JSONOutput/" + finalcoursecode + ".json");
+				 FileWriter file = new FileWriter(System.getProperty("user.dir") + "/PDFS/JSONOutput/" + finalcoursecode + currentyear + session + optionalcharacter + ".json");
 				 file.write(obj.toJSONString());
 				 file.flush();
 				 file.close();
@@ -181,65 +245,29 @@ public class App
 			 }
 			 
 		 }
-
+		 /*
+		 for (int i = 0; i < listOfFiles.length; i++){
+		 try{
+			 Files.move(Paths.get(listOfFiles[i].toString()), Paths.get(System.getProperty("user.dir") + "\\PDFS\\Syllabi\\" + listOfFiles[i].getName()));
+		 } catch (Exception e){
+			e.printStackTrace();
+		 }
+		 }
+		*/
 	 }
+	 /*
+	 public static void Move(File startingFile, String endinglocation){
+		 try{
+			 if(startingFile.renameTo(new File(endinglocation + startingFile.getName()))){
+				 System.out.println("File move successful!");
+			 } else{
+				 System.out.println("File move not successful");
+			 };
+		 } catch (Exception e){
+			 e.printStackTrace();;
+		 }
+	 }
+	 */
 }
-/*
- public static void main(String[] args) throws IOException {
-       PdfToText pdfManager = new PdfToText();
-       pdfManager.setFilePath(System.getProperty("user.dir") + "/src/main/java/com/soti/backend/swapnilParser/csSyllabus.pdf");
-       String rawtext = pdfManager.ToText();
-       System.out.println(rawtext);
-       String lines[] = rawtext.split("\\r?\\n");
-       String pattern = "(?<!\\S)(?:(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]|(?:Jan|Mar|May|Jul|Aug|Oct|Dec)))\\1|"
-       		+ "(?:(?:29|30)(\\/|-|\\.)(?:0?[1,3-9]|1[0-2]|(?:Jan|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec))\\2))"
-       		+ "(?:(?:1[6-9]|[2-9]\\d)?\\d{2})|(?:(?:1[6-9]|[2-9]\\d)?\\d{2})(\\/|-|\\.)(?:(?:(?:0?[13578]|1[02]|"
-       		+ "(?:Jan|Mar|May|Jul|Aug|Oct|Dec))\\3(?:31))|(?:(?:0?[1,3-9]|1[0-2]|(?:Jan|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)"
-       		+ ")\\3(?:29|30)))|(?:29(\\/|-|\\.)(?:0?2|(?:Feb))\\4(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|"
-       		+ "(?:(?:16|[2468][048]|[3579][26])00))))|(?:(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|"
-       		+ "[2468][048]|[3579][26])00)))(\\/|-|\\.)(?:0?2|(?:Feb))\\5(?:29))|(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9]|"
-       		+ "(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep))|(?:1[0-2]|(?:Oct|Nov|Dec)))\\6(?:(?:1[6-9]|[2-9]\\d)?\\d{2})|(?:(?:1[6-9]|"
-       		+ "[2-9]\\d)?\\d{2})(\\/|-|\\.)(?:(?:0?[1-9]|(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep))|(?:1[0-2]|(?:Oct|Nov|Dec)))\\7"
-       		+ "(?:0?[1-9]|1\\d|2[0-8]))(?!\\S)";
-       Pattern r = Pattern.compile(pattern);
-       List<String> matchedcharacters = new ArrayList<String>();
-
-       int count = 0;
-       for (String x : lines){
-    	   Matcher m = r.matcher(x);
-    	   while (m.find()){
-    		   count++;
-    		   matchedcharacters.add(x);
-    	   }   
-       }
-       
-       
-
-       //what do we do if the date is not valid?
-       
-       //properly parse the date into the correct format
-       List<DateTime> outputdates = new ArrayList<DateTime>();
-       Parser parser = new Parser();
-       for (String x : matchedcharacters){
-    	   List<DateGroup> group = parser.parse(x);
-    	   if (group.toString() == "[]"){
-    		   continue;
-    	   }
-    	   List<Date> date = (group.get(0)).getDates();
-           Date sampledate = date.get(0);
-           DateTime dt = new DateTime(sampledate);
-           outputdates.add(dt);
-           //System.out.println(dt.getMonthOfYear() + "-" + dt.getDayOfMonth() + "-" + dt.getYear());
-       }
-       
-       for (DateTime dt : outputdates){
-           System.out.println(dt.getMonthOfYear() + "-" + dt.getDayOfMonth() + "-" + dt.getYear());
-           //System.out.println(dt.getHourOfDay());
-       }
-       
 
 
-       
-       
-}
-*/
