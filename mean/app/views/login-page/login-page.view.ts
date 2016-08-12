4d1ff5fb5bc4d2150f70513654b0ca45d3467a9b
Adam Natale <adam.natale@soti.net>
2016-08-12 08:44:29 -0400

    #36: Refactor cat panels into their own component

--- mean/app/views/login-page/login-page.view.ts
+++ mean/app/views/login-page/login-page.view.ts
@@ -3,12 +3,13 @@
  */
 import {Component} from 'angular2/core';
 import {Schedule} from 'primeng/primeng';
+import {InfoPanelView} from './info-panel/info-panel.view';
 
 @Component({
     selector: 'sip-login-page',
     templateUrl: 'app/views/login-page/login-page.view.html',
     styleUrls: ['app/views/login-page/login-page.view.scss'],
-    directives: [Schedule]
+    directives: [Schedule, InfoPanelView]
 })
 export class LoginPageView {
     /*public element = document.getElementById('login-section');
