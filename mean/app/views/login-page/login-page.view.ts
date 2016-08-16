8bae86e68c2d9ce9e760ef48b481279ddc63cdc0
Adam Natale <adam.natale@soti.net>
2016-08-16 08:55:11 -0400

    #36: Added function headers to click events

--- mean/app/views/login-page/login-page.view.ts
+++ mean/app/views/login-page/login-page.view.ts
@@ -12,6 +12,34 @@ import {InfoPanelView} from './info-panel/info-panel.view';
     directives: [Schedule, InfoPanelView]
 })
 export class LoginPageView {
-    /*public element = document.getElementById('login-section');
-    element.addClass('form-success');*/
+    public username:string;
+    public password:string;
+
+    public sporeLogin(event: Event) {
+        console.log('Spore Login');
+    }
+
+    public sporeSignUp(event: Event) {
+        console.log('Spore SignUp');
+    }
+
+    public rememberMe(event: Event) {
+        console.log('Remember Me');
+    }
+
+    public forgotPassword(event: Event) {
+        console.log('Forgot Password?');
+    }
+
+    public facebookLogin(event: Event) {
+        console.log('Facebook Login');
+    }
+
+    public googleLogin(event: Event) {
+        console.log('Google Login');
+    }
+
+    public linkedinLogin(event: Event) {
+        console.log('LinkedIn Login');
+    }
 }
