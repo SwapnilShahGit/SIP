ca22e64687b20c65c57e36a2f70484f2c3c21b5b
Darren Hobin <darren.hobin@soti.net>
2016-08-26 15:29:46 -0400

    Fix mongo connection

--- Spore/server.js
+++ Spore/server.js
@@ -1,19 +1,21 @@
 var restify = require('restify');
 var mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost/test');
+
 var exec = require('child_process').exec;
 var Cat = mongoose.model('Cat', { name: String });
 
 function respond(req, res, next) {
   var kitty = new Cat({ name: req.params.name });
   kitty.save(function (err) {
-  if (err) {
-    res.send(err);
-  } else {
-    res.send('meow');
-  }
-});
-  res.send('Saved');
+    if (err) {
+      res.send(err);
+    } else {
+      res.send('meow');
+    }
+    res.send('Saved');
+  });
   next();
 }
 
@@ -27,14 +29,14 @@ function fetchCat(req, res, next) {
 function java(req, res, next) {
   var child = exec('java -jar Parser-jar-with-dependencies.jar');
   child.stdout.on('data', function(data) {
-	    console.log(data);
-
-	  res.send(data);
+	  console.log(foo);
+	  res.send(foo);
   });
 }
 
 var server = restify.createServer();
 server.get('/save/:name', respond);
+server.post('/save/:name', respond);
 server.head('/save/:name', respond);
 server.get('/get/:name', fetchCat);
 server.head('/get/:name', fetchCat);
