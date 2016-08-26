0bca8d0870c68c22b74dfc62e0ce13e53b9b8f05
Pavlo Usoltsev <paulusoltsev@gmail.com>
2016-08-25 23:49:11 -0400

    #44: Skeleton is set up

--- mean/app/app.component.ts
+++ mean/app/app.component.ts
@@ -3,12 +3,13 @@ import {Schedule} from 'primeng/primeng';
 import {CalendarComponent} from './components/calendar/calendar.component';
 import {StaticBarComponent} from './components/static-bar/static-bar.component';
 import {LoginPageView} from './views/login-page/login-page.view';
+import {MainPageView} from './views/main-page/main-page.view';
 
 @Component({
 	selector: 'my-app',
     templateUrl: 'app/app.component.html',
     styleUrls: ['app/app.component.scss'],
-    directives: [Schedule, CalendarComponent, StaticBarComponent, LoginPageView]
+    directives: [Schedule, CalendarComponent, StaticBarComponent, LoginPageView, MainPageView]
 })
 export class AppComponent {
 
