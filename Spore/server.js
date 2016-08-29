040a9c679f02908fa51a5d94195c634f930703ee
Swapnil Shah <swapnil.shah@soti.net>
2016-08-29 15:13:59 -0400

    fixing schema file

--- Spore/server.js
+++ Spore/server.js
@@ -1,40 +1,5 @@
 var restify = require('restify');
 var mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/test');
-
-var exec = require('child_process').exec;
-var Cat = mongoose.model('Cat', { name: String });
-
-function respond(req, res, next) {
-  var kitty = new Cat({ name: req.params.name });
-  kitty.save(function (err) {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send('meow');
-    }
-    res.send('Saved');
-  });
-  next();
-}
-
-function fetchCat(req, res, next) {
-  Cat.find({name: req.params.name}, function(err, cats){
-	if (err) return res.send(err);
-	res.send(cats);
-  });
-}
-
-function java(req, res, next) {
-  var child = exec('java -jar Parser-jar-with-dependencies.jar');
-  child.stdout.on('data', function(data) {
-	  console.log(foo);
-	  res.send(foo);
-  });
-}
-var restify = require('restify');
-var mongoose = require('mongoose');
 Schema = mongoose.Schema;
 
 var connection = mongoose.connect('mongodb://localhost/test');
@@ -101,6 +66,14 @@ function fetchInformation(req, res, next) {
        if (err) return res.send(err);
        res.send(cats);
   });
+
+function java(req, res, next) {
+  var child = exec('java -jar Parser-jar-with-dependencies.jar');
+  child.stdout.on('data', function(data) {
+	  console.log(foo);
+	  res.send(foo);
+  });
+}
 
 
 var server = restify.createServer();
