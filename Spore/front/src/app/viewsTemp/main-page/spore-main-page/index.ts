2b36c89ff5330534977c7316b4a677f2ec081bd4
Adam Natale <adam.natale@mail.utoronto.ca>
2016-08-31 22:53:02 -0400

    #105: Readded files on main page ui

---
+++ Spore/front/src/app/viewsTemp/main-page/spore-main-page/index.ts
@@ -0,0 +1,1 @@
+export * from './spore-main-page.component';
---
+++ Spore/front/src/app/viewsTemp/main-page/spore-main-page/spore-main-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-spore-main-page',
+  templateUrl: 'spore-main-page.component.html',
+  styleUrls: ['spore-main-page.component.css']
+})
+export class SporeMainPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
