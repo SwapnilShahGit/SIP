51d0486b4f9ee098ee592fd57bb7c73b228edc50
Pavlo Usoltsev <pavlo.usoltsev@soti.net>
2016-09-22 17:31:30 -0400

    #44 added some more stuff

--- Spore/front/src/app/viewsTemp/main-page/spore-main-page/spore-main-page.component.ts
+++ Spore/front/src/app/viewsTemp/main-page/spore-main-page/spore-main-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   moduleId: module.id,
@@ -6,11 +6,61 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: 'spore-main-page.component.html',
   styleUrls: ['spore-main-page.component.css']
 })
+
 export class SporeMainPageComponent implements OnInit {
 
-  constructor() { }
+  private _leftSlideOut: boolean = false;
+  private _rightSlideOut: boolean = false;
+  private _notificationsVisible: boolean = true;
 
-  ngOnInit() {
+  constructor() {
   }
 
+  events: any[];
+
+  ngOnInit() {
+      this.events = [
+        {
+          "title": "All Day Event",
+          "start": "2016-01-01"
+        },
+        {
+          "title": "Long Event",
+          "start": "2016-01-07",
+          "end": "2016-01-10"
+        }
+      ]
+  }
+
+  public setLeftSlide(): string {
+    return this._leftSlideOut ? 'visible' : 'hidden';
+  }
+
+  public setLeftDownSlide(): string {
+    return this._leftSlideOut ? 'hidden' : 'visible';
+  }
+
+  public setRightSlide(): string {
+    return this._rightSlideOut ? 'visible' : 'hidden';
+  }
+
+  public setRightDownSlide(): string {
+    return this._rightSlideOut ? 'hidden': 'visible';
+  }
+
+  public leftSlideButton() {
+    this._leftSlideOut = ! this._leftSlideOut;
+  }
+
+  public rightSlideButton() {
+    this._rightSlideOut = ! this._rightSlideOut;
+  }
+
+  public setNotificationsVisibility() {
+    this._notificationsVisible = ! this._notificationsVisible;
+  }
+
+  public notificationsVisibility(): string {
+    return this._notificationsVisible ? 'visible' : 'hidden';
+  }
 }
