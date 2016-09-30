9a89557ddafbd9b03c23974b88d556ffb8840152
usoltsevpaul <paulusoltsev@gmail.com>
2016-09-30 17:44:37 -0400

    Adding some base code for possible refactoring

---
+++ Spore/start/app/components/loginPage/loginPage.component.ts
@@ -0,0 +1,10 @@
+import { Component } from 'angular2/core';
+
+@Component({
+    selector: 'login-page',
+    templateUrl: './app/components/loginPage/loginPage.component.html',
+    styleUrls: ['./app/components/loginPage/loginPage.component.css']
+})
+export class LoginPageComponent {
+    
+ }
