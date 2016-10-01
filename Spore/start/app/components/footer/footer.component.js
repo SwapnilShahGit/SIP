18d3faf2f2e195db1e78333930b81d35239e316d
Pavlo Usoltsev <pavlo.usoltsev@soti.net>
2016-10-01 16:48:10 -0400

    adding components and stuff to refactoring branch

--- Spore/start/app/components/footer/footer.component.js
+++ Spore/start/app/components/footer/footer.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', './components/loginPage/loginPage.component'], function(exports_1, context_1) {
+System.register(['angular2/core'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,32 +10,29 @@ System.register(['angular2/core', './components/loginPage/loginPage.component'],
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, loginPage_component_1;
-    var AppComponent;
+    var core_1;
+    var FooterComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
-            },
-            function (loginPage_component_1_1) {
-                loginPage_component_1 = loginPage_component_1_1;
             }],
         execute: function() {
-            AppComponent = (function () {
-                function AppComponent() {
+            FooterComponent = (function () {
+                function FooterComponent() {
                 }
-                AppComponent = __decorate([
+                FooterComponent = __decorate([
                     core_1.Component({
-                        selector: 'main-component-app',
-                        templateUrl: './app/app.component.html',
-                        directives: [loginPage_component_1.LoginPageComponent]
+                        selector: 'spore-footer',
+                        templateUrl: './app/components/footer/footer.component.html',
+                        styleUrls: ['./app/components/footer/footer.component.css'],
                     }), 
                     __metadata('design:paramtypes', [])
-                ], AppComponent);
-                return AppComponent;
+                ], FooterComponent);
+                return FooterComponent;
             }());
-            exports_1("AppComponent", AppComponent);
+            exports_1("FooterComponent", FooterComponent);
         }
     }
 });
-//# sourceMappingURL=app.component.js.map
+//# sourceMappingURL=footer.component.js.map
--- Spore/start/app/components/loginPage/loginPage.component.ts
+++ Spore/start/app/components/loginPage/loginPage.component.ts
@@ -1,10 +1,45 @@
-import { Component } from 'angular2/core';
+import { Component, OnInit } from 'angular2/core';
+import { SporeInfoCardComponent } from '../spore-info-card/spore-info-card.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
     selector: 'login-page',
     templateUrl: './app/components/loginPage/loginPage.component.html',
-    styleUrls: ['./app/components/loginPage/loginPage.component.css']
+    styleUrls: ['./app/components/loginPage/loginPage.component.css'],
+    directives: [SporeInfoCardComponent, FooterComponent]
 })
-export class LoginPageComponent {
-    
+export class LoginPageComponent implements OnInit {
+  public username:string;
+  public password:string;
+
+  ngOnInit() {
+  }
+
+  public sporeLogin(event: Event) {
+    console.log('Spore Login');
+  }
+
+  public sporeSignUp(event: Event) {
+    console.log('Spore SignUp');
+  }
+
+  public rememberMe(event: Event) {
+    console.log('Remember Me');
+  }
+
+  public forgotPassword(event: Event) {
+    console.log('Forgot Password?');
+  }
+
+  public facebookLogin(event: Event) {
+    console.log('Facebook Login');
+  }
+
+  public googleLogin(event: Event) {
+    console.log('Google Login');
+  }
+
+  public linkedinLogin(event: Event) {
+    console.log('LinkedIn Login');
+  }
  }
