4401b2d57f1acdfa52a75cdd9a75e91bf08f581b
Adam Natale <adam.natale@soti.net>
2016-10-09 15:19:20 -0400

    Upgraded to .scss; Added main-page component.
    Upgraded to scss as it has more functionality than css; Added the main page component to have the class available. (Forgot to commit it on it's own, sorry Darren)
    
    Issue: #151

--- Spore/spore-angular/Spore/src/app/app.module.ts
+++ Spore/spore-angular/Spore/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { InfoCardComponent } from './components/info-card/info-card.component';
 import { FooterBarComponent } from './components/footer-bar/footer-bar.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
     InfoCardComponent,
-    FooterBarComponent
+    FooterBarComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ Spore/spore-angular/Spore/src/app/components/main-page/main-page.component.ts
@@ -0,0 +1,80 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.scss']
+})
+export class MainPageComponent implements OnInit {
+
+  private _leftSlideOut: boolean = false;
+  private _rightSlideOut: boolean = false;
+  private _notificationsVisible: boolean = true;
+  private _notificationCount: number = 0;
+
+  constructor() {
+  }
+
+  events: any[];
+
+  ngOnInit() {
+    this.events = [
+      {
+        "title": "All Day Event",
+        "start": "2016-01-01"
+      },
+      {
+        "title": "Long Event",
+        "start": "2016-01-07",
+        "end": "2016-01-10"
+      }
+    ]
+  }
+
+  public navigateToLoginPage() {
+    // this._router.navigate(['/login']);
+  }
+
+  public notifiedUser($event: Event) {
+    this._notificationCount += 1;
+  }
+
+  public temp() {
+    if (this._notificationCount > 9) {
+      return '9+';
+    }
+    return this._notificationCount.toString();
+  }
+
+  public setLeftSlide(): string {
+    return this._leftSlideOut ? 'visible' : 'hidden';
+  }
+
+  public setLeftDownSlide(): string {
+    return this._leftSlideOut ? 'hidden' : 'visible';
+  }
+
+  public setRightSlide(): string {
+    return this._rightSlideOut ? 'visible' : 'hidden';
+  }
+
+  public setRightDownSlide(): string {
+    return this._rightSlideOut ? 'hidden': 'visible';
+  }
+
+  public leftSlideButton() {
+    this._leftSlideOut = ! this._leftSlideOut;
+  }
+
+  public rightSlideButton() {
+    this._rightSlideOut = ! this._rightSlideOut;
+  }
+
+  public setNotificationsVisibility() {
+    this._notificationsVisible = ! this._notificationsVisible;
+  }
+
+  public notificationsVisibility(): string {
+    return this._notificationsVisible ? 'visible' : 'hidden';
+  }
+}
