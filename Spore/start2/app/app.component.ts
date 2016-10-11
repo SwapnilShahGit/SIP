6f965b9bcea385e5e5ebb69dfa847f2cbf16c4bc
Adam Natale <adam.natale@soti.net>
2016-10-11 09:25:02 -0400

    Moved upgraded project to the front folder, deleted all other folders with front end files.
    Moved the upgraded project to be in the front folder as it is the new (and hopefully final) project structure.
    
    Issue: #151

--- Spore/start2/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'pm-app',
-    templateUrl: './app/app.component.html'
-})
-export class AppComponent {
-    pageTitle: string = 'Spore';
-}
--- Spore/start2/app/app.routing.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var router_1 = require('@angular/router');
-var login_component_1 = require('./components/loginPage/login.component');
-var appRoutes = [
-    { path: '', redirectTo: 'login', pathMatch: 'full' },
-    { path: 'login', component: login_component_1.LoginComponent }
-];
-exports.routing = router_1.RouterModule.forRoot(appRoutes);
-//# sourceMappingURL=app.routing.js.map
