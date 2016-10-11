503663c0006628559367a86b9abe0b5a432e51de
Pavlo Usoltsev <pavlo.usoltsev@soti.net>
2016-10-11 16:45:22 -0400

    #158 added F.A. and styled the nav-bar

--- Spore/front/src/app/app.module.ts
+++ Spore/front/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { InfoCardComponent } from './components/info-card/info-card.component';
 import { FooterBarComponent } from './components/footer-bar/footer-bar.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { AppRoutingModule } from './app.routing';
+import { StaticNavBar } from './components/static-nav/static-nav.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { AppRoutingModule } from './app.routing';
     LoginPageComponent,
     InfoCardComponent,
     FooterBarComponent,
-    MainPageComponent
+    MainPageComponent,
+    StaticNavBar
   ],
   imports: [
     BrowserModule,
