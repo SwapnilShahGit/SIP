dff50d52d5cb8945724cf5f0b3bec43c21bcd7fa
usoltsevpaul <paulusoltsev@gmail.com>
2016-10-21 16:04:35 -0400

    #178 adding code for side nav (works ok now)

--- Spore/front/src/app/components/main-page/main-page.component.ts
+++ Spore/front/src/app/components/main-page/main-page.component.ts
@@ -8,23 +8,40 @@ import { FooterBarComponent } from '../footer-bar/footer-bar.component';
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss']
 })
-export class MainPageComponent {
+export class MainPageComponent implements OnInit {
+
+  private SelectedSchool: string = "Please select your school";
+
+  ngOnInit() {
+    this.openNav();
+  }
 
   constructor(private _router: Router) {
   }
 
   public navigateToLoginPage() {
     this._router.navigate(['/login']);
+  } 
+
+  public selectUofT() {
+    this.SelectedSchool = "University of Toronto";
   }
 
-  /* Set the width of the side navigation to 250px */
+  public selectSheridan() {
+    this.SelectedSchool = "Sheridan College";
+  }
+
+  public selectNoSchool() {
+    this.SelectedSchool = "Ain't nobody got time for that";
+  }
+
   public openNav() {
     document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
   }
 
-  /* Set the width of the side navigation to 0 */ 
   public closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
   }
-  
 }
