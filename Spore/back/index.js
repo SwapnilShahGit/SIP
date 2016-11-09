b6978679317b18ddc4dfc001a4ba0846c57d2ba8
Pranali Rathi <pranali.rathi@soti.net>
2016-11-09 16:34:20 -0500

    renamed testController to dbController [#170]

--- Spore/back/index.js
+++ Spore/back/index.js
@@ -5,7 +5,7 @@
 var restify = require('restify');
 var exec = require('child_process').exec;
 var fs = require('fs');
-var testController = require ('./test/testController')();
+var dbController = require('./test/dbController')();
 
 // -- set up server
 var server = restify.createServer();
@@ -14,82 +14,82 @@ var server = restify.createServer();
 
 // -- create and save user into 'test'
 function  testSaveUser(req, res, next){
-	var userInfo = {
-			userID: req.query.userID,
-			fName: req.query.fName,
-			lName: req.query.lName,
-			email: req.query.email,
-			school: req.query.school,
-			pass: req.query.pass,
-			eventID: req.query.eventID,
-			courses: req.query.courses
-	};
-	testController.saveUser(userInfo, function(stat){
-		res.send(stat);
-		next();
-	});
+  var userInfo = {
+    userID: req.query.userID,
+    fName: req.query.fName,
+    lName: req.query.lName,
+    email: req.query.email,
+    school: req.query.school,
+    pass: req.query.pass,
+    eventID: req.query.eventID,
+    courses: req.query.courses
+  };
+  dbController.saveUser(userInfo, function(stat){
+    res.send(stat);
+    next();
+  });
 
 }
 
 // -- fetch user info from 'test' given user id
 function  testFetchUser(req, res, next){
-	console.log("fetching user with id:" + req.query.user);
-	testController.fetchUser(req.query.user, function(err, user){
-		if (user !=null){
-			res.send(user);
-		}
-		else{
-			res.send("Error fetching user, err: "+ err);
-		}	
-		next();
-	});
+  console.log("fetching user with id:" + req.query.userID);
+  dbController.fetchUser(req.query.userID, function(err, user){
+    if (user !=null){
+      res.send(user);
+    }
+    else{
+      res.send("Error fetching user, err: "+ err);
+    } 
+    next();
+  });
 
 }
 
 // -- create and save event into 'test'
 function  testSaveEvent(req, res, next){
-	var eventInfo = {
-		startTime: req.query.startTime,
-		endTime: req.query.endTime,
-		title: req.query.title,
-		description: req.query.description,
-		type: req.query.type,
-		derivedFrom: req.query.derivedFrom
-	};
-	testController.saveEvent(eventInfo, function (stat){
-		res.send(stat);
-		next();
-	});
+  var eventInfo = {
+    startTime: req.query.startTime,
+    endTime: req.query.endTime,
+    title: req.query.title,
+    description: req.query.description,
+    type: req.query.type,
+    derivedFrom: req.query.derivedFrom
+  };
+  dbController.saveEvent(eventInfo, function (stat){
+    res.send(stat);
+    next();
+  });
 }
 
 // -- fetch event info from 'test' given event id
 function  testFetchEvent(req, res, next){
-	console.log("fetching event with id:" + req.query.eventID);
-	testController.fetchEvent(req.query.eventID, function(err, Event){
-		if (Event !=null){
-			res.send(Event);
-		}
-		else{
-			res.send("Error fetching event, err: " + err);
-		}
-		next();
-	});
+  console.log("fetching event with id:" + req.query.eventID);
+  dbController.fetchEvent(req.query.eventID, function(err, Event){
+    if (Event !=null){
+      res.send(Event);
+    }
+    else{
+      res.send("Error fetching event, err: " + err);
+    }
+    next();
+  });
 }
 
 // -- execute the parser to process JSON files
 function java(req, res, next) {
-	var child = exec('java -jar ../parser/Parser-jar-with-dependencies.jar');
-	child.stdout.on('data', function(data) {
-		console.log(data.toString('utf8'));
-		res.send(data.toString('utf8'));
-	});
-	next();
+  var child = exec('java -jar ../parser/Parser-jar-with-dependencies.jar');
+  child.stdout.on('data', function(data) {
+    console.log(data.toString('utf8'));
+    res.send(data.toString('utf8'));
+  });
+  next();
 }
 
 // -- echo the input
 function echoValue(req, res, next) {
-	res.send(req.query.value);
-	next();
+  res.send(req.query.value);
+  next();
 }
 
 // -- END REQUEST HANDLERS ------------------------------------------------------------------------
