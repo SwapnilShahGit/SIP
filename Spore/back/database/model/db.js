65b1404446d65284c1941b7ad3760d610f31b703
Pranali Rathi <pranali.rathi@soti.net>
2016-11-09 17:44:31 -0500

    Separate server.js into proper components
    Server.js is now split into 3 different files: index.js, testcontroller.js
    and db.js. index.js is the main file that contains routing http requests,
    restify module, the parser. dbController.js is the controller for the
    all the functions that interact with the database. db.js is the file that
    actually starts the database and holds all schemas and models and is the
    only file that directly touches the database.
    
    [#170]

---
+++ Spore/back/database/model/db.js
@@ -0,0 +1,86 @@
+//_________________________________________________________________________________________________
+// -- Handles events relating to the database connection and holds the schema and model definitions 
+// -- that will be used for the 'test' database.  
+// -- Created October 11, 2016
+//_________________________________________________________________________________________________
+
+var mongoose = require('mongoose');
+var autoIncrement = require('mongoose-auto-increment');
+
+module.exports = function () {
+  // -- create and start a connection to database named 'test' on localhost
+  var dbURI = 'mongodb://localhost/test'; 
+  var connection = mongoose.connect(dbURI);
+  
+  var Schema = mongoose.Schema;
+  autoIncrement.initialize(connection);
+
+  // -- CONNECTION EVENTS -----------------------------------------------------------------------
+  
+  // -- when successfully connected
+  mongoose.connection.on( 'connected', function() {
+    console.log('Mongoose connected to ' + dbURI);
+  });
+  
+  // -- when connection throws an error 
+  mongoose.connection.on( 'error', function( err ) {
+    console.log('Mongoose connection error: ' + err);
+  });
+  
+  // -- when connection is disconnected
+  mongoose.connection.on( 'disconnected', function() {
+    console.log('Mongoose connection disconnected');
+  });
+  
+  // -- END CONNECTION EVENTS -------------------------------------------------------------------
+  
+  // -- create schema for table that will hold user information
+  var usersTable = new Schema({
+    UserID: {type: String, unique: true},
+    FirstName: String,
+    LastName: String,
+    Email: String,
+    School: String,
+    Password: String,
+    EventID: [Number],
+    Courses: [String]
+  });
+  
+  // -- create schema that will hold course information
+  var syllabusLibrary = new Schema({
+    Hash: String,
+    CourseID: String,
+    ParsedInfo: String,
+    ReferenceNumber: Number,
+    EventIDList: [Number],
+    ExamEventID: Number
+  });
+  
+  // -- create schema that will hold event information
+  var eventLibrary = new Schema({
+    StartTime: Number,
+    EndTime: Number,
+    Title: String,
+    Description: String,
+    Type: String,
+    DerivedFrom: Number,
+    EventID: {type: Number, unique: true}
+  });
+  
+  // -- generate auto-incrementing event IDs 
+  eventLibrary.plugin(autoIncrement.plugin, {
+    model: 'Event',
+    field: 'EventID',
+    startAt: 0,
+    incrementBy: 1
+  }); 
+
+
+  var usersTable = mongoose.model('usersTable', usersTable);
+  var syllabusLibrary = mongoose.model('syllabusLibrary', syllabusLibrary);
+  var eventLibrary = mongoose.model('eventLibrary', eventLibrary);
+    
+  var exports = {};
+  exports.mongoose = mongoose;
+  return exports;
+}
