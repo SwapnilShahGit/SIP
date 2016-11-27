be2f4f6cd6348859e0bb2e7806afdc948ab2f201
Darren Hobin <darren.hobin@soti.net>
2016-11-27 16:32:29 -0500

    Ease use of start script
    The start script will not fallback to a default database path if one is
    not present. Further, given the script will crash on anything but
    Windows, the script now gracefully fails if ran on Linux or macOS.
    
    Logging was also updated to provide more accurate information.
    
    [#210]

--- start.sh
+++ start.sh
@@ -1,19 +1,43 @@
 #!/bin/bash
+
+MONGO_HOME="C:/Program Files/MongoDB"
+DEFAULT_DB="C:/data/db"
+
 RED=$(tput setaf 1)
 UNDERLINE=$(tput smul)
 NORMAL=$(tput sgr0)
 
-mongo="C:/Program Files/MongoDB"
-data=$1
+KERNEL=$(uname -s)
+
+# Fail gracefully is user attempts to run on Linux or macOS
+if [ "${KERNEL^^}" == "LINUX" -o "${KERNEL^^}" == "DARWIN" ]; then
+    printf "\n${RED}This script only supports Bash on Windows.${NORMAL}\n"
+    exit 1
+fi
+
+mongo="$MONGO_HOME"
+if [ -z $1 ]; then
+    data="$DEFAULT_DB"
+else
+    data="$1"
+fi
+
 printf "\n${UNDERLINE}Starting Mongo...${NORMAL}\n"
 "$mongo/Server/3.2/bin/mongod.exe" --dbpath "$data" > /dev/null &
-printf "\n${UNDERLINE}Deleting Current Dist Folder...${NORMAL}\n"
+
+printf "\n${UNDERLINE}Deleting front/dist...${NORMAL}\n"
 cd Spore/front
 rm -rf dist
+
+printf "\n${UNDERLINE}Installing node_modules for front...${NORMAL}\n"
 npm install
-printf "\n${UNDERLINE}Creating new build...${NORMAL}\n"
+
+printf "\n${UNDERLINE}Creating new build for front...${NORMAL}\n"
 npm run build:prod
+
+printf "\n${UNDERLINE}Installing node_modules for back...${NORMAL}\n"
 cd ../back
 npm install
+
 printf "\n${UNDERLINE}Starting Spore...${NORMAL}\n"
 npm start
