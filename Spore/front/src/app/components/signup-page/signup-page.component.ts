eb87193e1e0ef573f86029c48595f79126d366b9
Adam Natale <adam.natale@soti.net>
2016-12-05 14:45:52 -0500

    Continued styling the sign up page
    More progress to the sign up page
    
    #324

--- Spore/front/src/app/components/signup-page/signup-page.component.ts
+++ Spore/front/src/app/components/signup-page/signup-page.component.ts
@@ -1,8 +1,10 @@
 /**
  * Created by anatale on 12/1/2016.
  */
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import * as _ from 'lodash';
+import {SupportedSchoolsEnum} from '../../../meta/SupportedSchools';
 
 @Component({
   selector: 'signup-page',
@@ -10,9 +12,19 @@ import {Router} from '@angular/router';
   styleUrls: ['./signup-page.component.scss']
 })
 
-export class SignUpPageComponent {
+export class SignUpPageComponent implements OnInit{
 
-  constructor(private router: Router){}
+  public supportedSchools;
+
+  constructor(private router: Router){
+    this.supportedSchools = _.flatMap(SupportedSchoolsEnum);
+  }
+
+  public ngOnInit() {
+    this.supportedSchools = _.flatMap(SupportedSchoolsEnum);
+    console.log(this.supportedSchools);
+    console.log(SupportedSchoolsEnum);
+  }
 
   sporeLogin(event: Event) {
     this.router.navigate(['/login']);
