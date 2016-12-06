2b9e74e53e257b37398f0e1cd2304b0d69b17e04
Adam Natale <adam.natale@soti.net>
2016-12-06 09:13:50 -0500

    Finished sign up page styling
    Added dropdown to date selector to select month/year, cleaned up ui elements
    
    [#324]

--- Spore/front/src/meta/SupportedSchools.ts
+++ Spore/front/src/meta/SupportedSchools.ts
@@ -5,7 +5,7 @@
 export enum SupportedSchoolsEnum {
   'University of Toronto St. George',
   'University of Toronto Mississauga',
-  'Universtiy of Toronto Scarbarough',
+  'Universtiy of Toronto Scarborough',
   'University Of Waterloo',
   'Other'
 }
