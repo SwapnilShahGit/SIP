73d74f797b27e06a9fa18963cbabc04b0891464c
usoltsevpaul <paulusoltsev@gmail.com>
2016-12-06 17:43:54 -0500

    #348 added initial calendar functionality

--- Spore/front/src/app/components/main-page/main-page.component.ts
+++ Spore/front/src/app/components/main-page/main-page.component.ts
@@ -35,7 +35,7 @@ export class MainPageComponent implements OnInit, OnDestroy {
     private databaseService: DatabaseService,
     private navService: NavService
   ) {
-    this.CurrentTab = navService.HelpTab;
+    this.CurrentTab = navService.CaledarTab;
     this.subscription = navService.navOpen$.subscribe(
       isOpen => {
         if (isOpen) {
@@ -47,68 +47,68 @@ export class MainPageComponent implements OnInit, OnDestroy {
     );
   }
 
-  calendarOptions: Object = {
-    fixedWeekCount: false,
-    defaultDate: '2016-09-12',
-    editable: true,
-    eventLimit: true, // allow "more" link when too many events
-    events: [
-      {
-        title: 'All Day Event',
-        start: '2016-09-01'
-      },
-      {
-        title: 'Long Event',
-        start: '2016-09-07',
-        end: '2016-09-10'
-      },
-      {
-        id: 999,
-        title: 'Repeating Event',
-        start: '2016-09-09T16:00:00'
-      },
-      {
-        id: 999,
-        title: 'Repeating Event',
-        start: '2016-09-16T18:00:00'
-      },
-      {
-        title: 'Conference',
-        start: '2016-09-11',
-        end: '2016-09-13'
-      },
-      {
-        title: 'Meeting',
-        start: '2016-09-12T10:30:00',
-        end: '2016-09-12T12:30:00'
-      },
-      {
-        title: 'Lunch',
-        start: '2016-09-12T12:00:00'
-      },
-      {
-        title: 'Meeting',
-        start: '2016-09-12T14:30:00'
-      },
-      {
-        title: 'Happy Hour',
-        start: '2016-09-12T17:30:00'
-      },
-      {
-        title: 'Dinner',
-        start: '2016-09-12T20:00:00'
-      },
-      {
-        title: 'Birthday Party',
-        start: '2016-09-13T07:00:00'
-      },
-      {
-        title: 'Click for Google',
-        url: 'http://google.com/',
-        start: '2016-09-28'
-      }
-    ]
-  };
+  header: any = {
+	  left: 'prev,next today',
+	  center: 'title',
+	  right: 'month,agendaWeek,agendaDay'
+	};
+
+  events: any = [
+    {
+      title: 'All Day Event',
+      start: '2016-09-01'
+    },
+    {
+      title: 'Long Event',
+      start: '2016-09-07',
+      end: '2016-09-10'
+    },
+    {
+      id: 999,
+      title: 'Repeating Event',
+      start: '2016-09-09T16:00:00'
+    },
+    {
+      id: 999,
+      title: 'Repeating Event',
+      start: '2016-09-16T18:00:00'
+    },
+    {
+      title: 'Conference',
+      start: '2016-09-11',
+      end: '2016-09-13'
+    },
+    {
+      title: 'Meeting',
+      start: '2016-09-12T10:30:00',
+      end: '2016-09-12T12:30:00'
+    },
+    {
+      title: 'Lunch',
+      start: '2016-09-12T12:00:00'
+    },
+    {
+      title: 'Meeting',
+      start: '2016-09-12T14:30:00'
+    },
+    {
+      title: 'Happy Hour',
+      start: '2016-09-12T17:30:00'
+    },
+    {
+      title: 'Dinner',
+      start: '2016-09-12T20:00:00'
+    },
+    {
+      title: 'Birthday Party',
+      start: '2016-09-13T07:00:00'
+    },
+    {
+      title: 'Click for Google',
+      url: 'http://google.com/',
+      start: '2016-09-28'
+    }
+  ];
 
   ngOnInit() {
     this.user = this.databaseService.user;
@@ -119,6 +119,14 @@ export class MainPageComponent implements OnInit, OnDestroy {
         this.databaseService.loadUser(this.userID);
       }
     });
+  }
+
+  handleEventClick(event) {
+    console.log("event clicked");
+  }
+
+  handleDayClick(event) {
+    console.log("day clicked");
   }
 
   echoTester() {
