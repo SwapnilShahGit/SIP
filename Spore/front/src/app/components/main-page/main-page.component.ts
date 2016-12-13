0c5993e78e0282b7aa13b43ef518e3be8aee709d
usoltsevpaul <paulusoltsev@gmail.com>
2016-12-13 08:24:28 -0500

    #357 calendar new component +cleanup+ spellingfix (#361)

--- Spore/front/src/app/components/main-page/main-page.component.ts
+++ Spore/front/src/app/components/main-page/main-page.component.ts
@@ -1,14 +1,11 @@
 import { StaticNavBar } from '../static-nav/static-nav.component';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, ActivatedRoute, Params } from "@angular/router";
-import { FooterBarComponent } from '../footer-bar/footer-bar.component';
 import { Http, Response } from '@angular/http';
 import { DatabaseService } from '../../../meta/database.service';
 import { User } from '../../../meta/user';
-import { Event } from '../../../meta/event';
 import { Subscription } from 'rxjs/Subscription';
 import { NavService } from '../../../meta/nav.service';
-import { ScheduleModule, DialogModule } from 'primeng/primeng';
 import { Observable } from 'rxjs/Rx';
 
 @Component({
@@ -28,25 +25,6 @@ export class MainPageComponent implements OnInit, OnDestroy {
   subscription: Subscription;
   user: Observable<User>;
   userID: string;
-  event: Event;
-  dialogVisible: boolean = false;
-  header: any = {
-	  left: 'prev,next today',
-	  center: 'title',
-	  right: 'month,agendaWeek,agendaDay'
-	};
-
-  events: any = [
-    {
-      title: 'All Day Event',
-      start: '2016-09-01'
-    },
-    {
-      title: 'Long Event',
-      start: '2016-09-07',
-      end: '2016-09-10'
-    }
-  ];
 
   constructor(
     private router: Router,
@@ -54,7 +32,7 @@ export class MainPageComponent implements OnInit, OnDestroy {
     private databaseService: DatabaseService,
     private navService: NavService
   ) {
-    this.CurrentTab = navService.CaledarTab;
+    this.CurrentTab = navService.CalendarTab;
     this.subscription = navService.navOpen$.subscribe(
       isOpen => {
         if (isOpen) {
@@ -75,47 +53,7 @@ export class MainPageComponent implements OnInit, OnDestroy {
         this.databaseService.loadUser(this.userID);
       }
     });
-
-    /// Add a call to populate the calendar content here (or observe it)
   }
-
-  /// API calls need to be added -----------------------------------------------
-  handleEventClick(e) {
-    this.event = new Event();
-    this.event.Start = e.calEvent._start.toDate();
-    this.event.Start.setDate(this.event.Start.getDate() + 1); // have to add 1 day for some reason...
-    this.dialogVisible = true;
-    console.log("event clicked - " + e.calEvent.title);
-  }
-
-  handleDayClick(e) {
-    this.event = new Event();
-    this.event.Start = e.date.toDate();
-    this.event.Start.setDate(this.event.Start.getDate() + 1); // have to add 1 day for some reason...
-    this.dialogVisible = true;
-    console.log("day clicked - " + this.event.Start);
-  }
-
-  saveEvent() {
-    if (this.event.Id) {
-      // update call
-      console.log("update called");
-    } else {
-      // create call
-      console.log("create called");
-    }
-
-    this.dialogVisible = false;
-    this.event = undefined;
-  }
-
-  deleteEvent() {
-    // delete call
-    this.dialogVisible = false;
-    this.event = undefined;
-    console.log("delete called");
-  }
-  /// API calls need to be added -----------------------------------------------
 
   echoTester() {
     this.databaseService.echo(this.echoInput).then(data => this.echoResponse = data);
