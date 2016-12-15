773a9524e891b00764861b1a4dd1c7439fc41fc3
Swapnil Shah <swapnil.shah@mail.utoronto.ca>
2016-12-15 14:52:11 -0500

    add todo notes that show frontend theyre dumb
    [#294]

--- Spore/back/index.js
+++ Spore/back/index.js
@@ -421,30 +421,36 @@ location, contact, repeat){
 // -- execute the parser to process JSON files
 function java(req, res, next) {
   listOfCourseEvents = [];
-  // check to see if course being requested already exists in the db
-  
-  // if it exists
-    // find events related to that course and add them to listOfCourseEvents
+  // TODO: check to see if course being requested already exists in the db
+  //if dbController.checkCourseExists(req.query.id)
+  // TODO: if it exists
+    // TODO: find events related to that course and add them to listOfCourseEvents
   
   // if course DNE
+    
     var child = exec('java', ['-jar', '../parser/Parser-jar-with-dependencies.jar']);
     const rl = readline.createInterface({
       input: child.stdout,
       output: child.stdin
     })
+    
     rl.on('line', function(data) {
       var courseinfo = JSON.parse(data);
       dbController.javaSaveEvent(courseinfo.mongodbevents, function(stat){
       // add events to listOfCourseEvents
       listOfCourseEvents.concat(stat);
       });
-    // make a new course in the courses table with the newly generated id's
+    
+    // TODO: make a new course in the courses table with the newly generated id's
 
     });
-  
-  // add listOfCourseEvents to the user requesting this information
+  // TODO: check with the user to see which lecture/tutorial section they're in
+  // TODO: add listOfCourseEvents to the user requesting this information
+}
 
-
+function saveCoursetoUser(req, res, next){
+// TODO: function should take in userID, course code, lecture section, tutorial section,
+// and practical section that they're in
 }
 
 // -- echo the input
