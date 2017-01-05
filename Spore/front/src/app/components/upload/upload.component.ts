62606b67c5f895e880d2dc93703886181d21c3ea
Pavlo Usoltsev <pavlo.usoltsev@soti.net>
2017-01-05 16:31:50 -0500

    Create file upload UI
    These changes are required for uploading syllabi and other files to Spore.
    
    [#398]

---
+++ Spore/front/src/app/components/upload/upload.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { FileUploader } from 'ng2-file-upload';
+
+@Component({
+    selector: 'upload-factory',
+    templateUrl: './upload.component.html',
+    styleUrls: ['./upload.component.scss']
+})
+export class UploadComponent implements OnInit {
+
+    private server: string = ENV == "production" ? "https://spore.life" : "https://localhost:8081";
+    private maxFileSize = 1024 * 1024 * 15;
+    private uploader: FileUploader = new FileUploader({url: this.server + '/api/parse', maxFileSize: this.maxFileSize});
+    private hasBaseDropZoneOver: boolean = false;
+
+    constructor(
+      private changeDetector: ChangeDetectorRef
+    ) { }
+
+    ngOnInit() {
+      this.changeDetector.detectChanges();
+    }
+
+    public fileOverBase(e) {
+      this.hasBaseDropZoneOver = e;
+    }
+
+    startUpload(item?) {
+      this.uploader.queue.forEach(i => {
+        i.onProgress = (progress: any) => {
+          this.changeDetector.detectChanges();
+        }
+      });
+
+      if (item) {
+        this.uploader.uploadItem(item);
+      } else {
+        this.uploader.uploadAll();
+      }
+    }
+
+}
