5ef329cadb93575821f33840e07c11f2765eb32c
usoltsevpaul <paulusoltsev@gmail.com>
2017-01-10 17:13:30 -0500

    #410 added setting tab and cleaned up random code
     Not finished fully but is at a good point to merge to develop. Will
     continue on after merge though.

--- Spore/front/src/meta/user.ts
+++ Spore/front/src/meta/user.ts
@@ -10,8 +10,9 @@ export class User implements IUser {
   FacebookID: string;
   PictureURL: string;
   School: string;
+  Theme: string;
 
-  constructor(userID?: string, firstName?: string, lastName?: string, password?: string, email?: string, gender?: string, facebookId?: string, pic?: string, school?: string) {
+  constructor(userID?: string, firstName?: string, lastName?: string, password?: string, email?: string, gender?: string, facebookId?: string, pic?: string, school?: string, theme?: string) {
     this.UserID = userID || '' ;
     this.FirstName = firstName || '';
     this.LastName = lastName || '';
@@ -21,6 +22,7 @@ export class User implements IUser {
     this.FacebookID = facebookId || '';
     this.PictureURL = pic|| 'https://scontent.xx.fbcdn.net/v/t1.0-1/c15.0.50.50/p50x50/10354686_10150004552801856_220367501106153455_n.jpg?oh=6c801f82cd5a32fd6e5a4258ce00a314&oe=589AAD2F';
     this.School = school || '';
+    this.Theme = theme || 'Default';
   }
 
 }
@@ -34,6 +36,7 @@ export interface IUser {
   Gender: string;
   FacebookID: string,
   PictureURL: string,
-  School: string
+  School: string,
+  Theme: string
 }
 
