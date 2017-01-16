176c8f93cbf4a748ee5cfe8843ce5d8dc86c85a3
Pranali Rathi <pranali.rathi@soti.net>
2017-01-16 16:14:42 -0500

    script to create dummy data to populate db with events is complete. Script takes in 2 paramters: 1) number of events to be creates 2) userID [#404]

--- Spore/back/populateEvents.js
+++ Spore/back/populateEvents.js
@@ -5,43 +5,109 @@
 // -- Created January 5, 2017
 //_________________________________________________________________________________________________
 var dj = require('dummy-json');
-
-var eventArray = [];
-
-
- // -- create a list of events
-for (var i = 0; i<1000; i++){
-
-   var date = '{{date "2010-01-01T00:00" "2020-12-31T24:00" "YYYY-MM-DDThh:mm:ss"}}';
-   var fewHours ='{{int 1 3600000 round=1000}}';
-   var sameDay = '{{int 1 86400000 round=1000}}';
-   var differentDay = '{{int 86400000 604800000 round=1000}}';
-
-    var startTime = Date.parse(dj.parse(date));
-    var endTime ;
+var Promise = require ('bluebird');
+var mongoose = require('mongoose');
+var counter = 0;
 
 
-    // -- longer events will be created less frequently than
-    if ((i%100) == 0){
-        endTime = startTime + parseInt(dj.parse(differentDay));
-    }
-    else if((i%75) == 0){
-        endTime = startTime + parseInt(dj.parse(sameDay));
-    }
-    else{
-        endTime = startTime + parseInt(dj.parse(fewHours));
-    }
+// -- get script arguments
+var numOfEvents = process.argv[2]
+var userID = process.argv[3];
 
-    var eventTitle = dj.parse('{{lorem 10}}');
+// -- set up db connection:
+var DATABASE_URI = 'mongodb://localhost/spore';
 
-    var event = {
-         title: eventTitle,
-         start: new Date(startTime),
-         end: new Date(endTime)
-    }
-    eventArray[i] = event;
+mongoose.Promise = global.Promise;
 
+mongoose.connect(DATABASE_URI);
+
+mongoose.connection.on( 'connected', function() {
+	console.log('Mongoose connected to ' + DATABASE_URI);
+});
+
+mongoose.connection.on( 'error', function( err ) {
+	console.log('Mongoose connection error: ' + err);
+});
+
+mongoose.connection.on( 'disconnected', function() {
+	console.log('Mongoose connection disconnected');
+});
+
+
+
+// -- db schema/models
+var Schema = mongoose.Schema;
+
+var eventSchema = new Schema({
+	title: String,
+	start: Date,
+	end: Date,
+	background: String,
+	description: String,
+	location: String,
+	contact: String,
+	course: String,
+	repeat: Number
+});
+
+var userSchema = new Schema({
+	first: String,
+	last: String,
+	pass: String,
+	email: String,
+	gender: String,
+	facebook_id: String,
+	picture_uri: String,
+	event_ids: [Schema.Types.ObjectId],
+	school: String,
+	theme: String
+});
+
+
+// -- function to create give number of events and save into db
+function Populate(){
+	for (var i = 1; i <= numOfEvents; i++){
+		var user = mongoose.model('user', userSchema);
+		var event = mongoose.model('event', eventSchema);
+
+	   var date = '{{date "2015-01-01T00:00" "2020-12-31T24:00" "YYYY-MM-DDThh:mm:ss"}}';
+	   var fewHours ='{{int 1 3600000 round=1000}}';
+	   var sameDay = '{{int 1 86400000 round=1000}}';
+	   var differentDay = '{{int 86400000 604800000 round=1000}}';
+
+		var startTime = Date.parse(dj.parse(date));
+		var endTime ;
+
+		// -- longer events will be created less frequently than
+		if ((i%100) == 0){
+			//console.log("differentDay");
+			endTime = startTime + parseInt(dj.parse(differentDay));
+		}
+		else if((i%75) == 0){
+			//console.log("sameDay");
+			endTime = startTime + parseInt(dj.parse(sameDay));
+		}
+		else{
+			//console.log("fewHours");
+			endTime = startTime + parseInt(dj.parse(fewHours));
+		}
+
+		var eventTitle = dj.parse('{{lorem 10}}');
+
+		var event = new event({
+			 title: eventTitle,
+			 start: new Date(startTime),
+			 end: new Date(endTime)
+		})
+		event.save(event).then(Promise.coroutine(function* (doc) {
+			yield user.update({_id: userID}, {$push: {event_ids: doc.id}});
+			counter++;
+			if ((counter+1) == numOfEvents ) console.log("DONE");
+		})).catch(function(err) {
+		 console.log ("ERRROR Saving");
+		})
+	}
 }
-console.log(eventArray);
+Populate();
 
 
