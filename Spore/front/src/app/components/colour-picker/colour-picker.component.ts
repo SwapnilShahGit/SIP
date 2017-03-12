7fd73601c0b6f787780fbe38a1d460ecd6e849ff
Pavlo Usoltsev <pavlo.usoltsev@soti.net>
2017-03-12 17:48:44 -0400

    #478 moved colour picking logic to title bar

---
+++ Spore/front/src/app/components/colour-picker/colour-picker.component.ts
@@ -0,0 +1,49 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'custom-colour-picker',
+  templateUrl: './colour-picker.component.html',
+  styleUrls: ['./colour-picker.component.scss']
+})
+
+export class CustomColourPickerComponent {
+
+  private colourValue = "#0000ff";
+  private width: number = 25;
+  private height: number = 25;
+  private borderRadius: number = 50;
+  private defaultColours = [
+    '#33cccc',
+    '#99cc99',
+    '#cc99cc',
+    '#fabf8f',
+    '#bfbfbf',
+    '#6699ff',
+    '#ff6666',
+    '#ffcc66'
+  ];
+
+  @Input()
+  public dropdownClass;
+
+  @Input()
+  public buttonClass;
+
+  @Input()
+  public get colour() {
+    return this.colourValue;
+  }
+
+  @Output() colourChange = new EventEmitter();
+
+  public set colour(colour) {
+    this.colourValue = colour;
+    this.colourChange.emit(this.colourValue);
+  }
+
+  public setColour(colour) {
+    this.colourValue = colour;
+    this.colourChange.emit(this.colourValue);
+  }
+
+}
--- Spore/front/src/app/components/courses-page/courses-page.component.ts
+++ Spore/front/src/app/components/courses-page/courses-page.component.ts
@@ -12,31 +12,48 @@ export class CoursesPageComponent implements OnInit {
 
   private currentSlide: string = "";
   private courses: Array<Course> = [];
-  private basicColours: Array<String> = ['#ff7373', '#ffd700', '#3399ff', '#00ff00', '#ffa500', '#8a2be2'];
+  private coursesMaster: Array<Course> = [];
+  private deleteDialogVisible: boolean = false;
+  private deleteCourseCode: string = "";
+  private searchDialogVisible: boolean = false;
 
   public ngOnInit() {
-    var lectures: Array<CourseOption> = [new CourseOption('A', '1pm', 0)];
-    var tutorials: Array<CourseOption> = [new CourseOption('A', '1pm', 0), new CourseOption('B', '2pm', 1)];
-    var practicals: Array<CourseOption> = [new CourseOption('A', '1pm', 0), new CourseOption('B', '2pm', 1), new CourseOption('C', '3pm', 2)];
-    var course: Course = new Course(false, 'ABC123', 'Dr. Guy', 'No clue what this is...', null, 0, lectures, 0, tutorials, 0, practicals);
-    this.courses.push(course);
-    course = new Course(false, 'CSC309', 'Cool Dude Arnold', 'This is a pretty cool class.', 'csc309faspfsaopfjapas');
-    this.courses.push(course);
-    var tutorials1: Array<CourseOption> = [new CourseOption('A', '1pm', 0), new CourseOption('B', '2pm', 1)];
-    var tutorials2: Array<CourseOption> = [new CourseOption('A', '1pm', 0), new CourseOption('B', '2pm', 1)];
-    course = new Course(false, 'MAT111', 'Fuchs', 'This class is also pretty cool.', 'mat111fa124asfpfjapas', 1, tutorials1, 0, tutorials2);
-    this.courses.push(course);
-    var lectures1: Array<CourseOption> = [new CourseOption('A', '1pm', 0)];
-    var lectures2: Array<CourseOption> = [new CourseOption('A', '2pm', 0)];
-    var lectures3: Array<CourseOption> = [new CourseOption('A', '3pm', 0)];
-    course = new Course(false, 'ECO101', 'Bailey on Ice', 'Smooth as hell.', 'eco101ffas2aspfsaopfjapas', 0, lectures1, 0, lectures2, 0, lectures3);
-    this.courses.push(course);
+    // let lectures: Array<CourseOption> = [new CourseOption('A', '1pm', 0)];
+    // let tutorials: Array<CourseOption> = [new CourseOption('A', '1pm', 0), new CourseOption('B', '2pm', 1)];
+    // let practicals: Array<CourseOption> = [new CourseOption('A', '1pm', 0), new CourseOption('B', '2pm', 1), new CourseOption('C', '3pm', 2)];
+    // let course: Course = new Course(false, 'ABC123', 'Dr. Guy', 'No clue what this is...', null, 0, lectures, 0, tutorials, 0, practicals);
+    // this.courses.push(course);
+    // let courseMaster = <Course>JSON.parse(JSON.stringify(course));
+    // this.coursesMaster.push(courseMaster);
+    // course = new Course(false, 'CSC309', 'Cool Dude Arnold', 'This is a pretty cool class.', 'csc309faspfsaopfjapas');
+    // this.courses.push(course);
+    // courseMaster = <Course>JSON.parse(JSON.stringify(course));
+    // this.coursesMaster.push(courseMaster);
+    // let tutorials1: Array<CourseOption> = [new CourseOption('A', '1pm', 0), new CourseOption('B', '2pm', 1)];
+    // let tutorials2: Array<CourseOption> = [new CourseOption('A', '1pm', 0), new CourseOption('B', '2pm', 1)];
+    // course = new Course(false, 'MAT111', 'Fuchs', 'This class is also pretty cool.', 'mat111fa124asfpfjapas', 1, tutorials1, 0, tutorials2);
+    // this.courses.push(course);
+    // courseMaster = <Course>JSON.parse(JSON.stringify(course));
+    // this.coursesMaster.push(courseMaster);
+    // let lectures1: Array<CourseOption> = [new CourseOption('A', '1pm', 0)];
+    // let lectures2: Array<CourseOption> = [new CourseOption('A', '2pm', 0)];
+    // let lectures3: Array<CourseOption> = [new CourseOption('A', '3pm', 0)];
+    // course = new Course(false, 'ECO101', 'Bailey on Ice', 'Smooth as hell.', 'eco101ffas2aspfsaopfjapas', 0, lectures1, 0, lectures2, 0, lectures3);
+    // this.courses.push(course);
+    // courseMaster = <Course>JSON.parse(JSON.stringify(course));
+    // this.coursesMaster.push(courseMaster);
   }
 
   private updateCourse(course: Course): any {
     console.log('Course Update Called!');
     console.log(course);
     // API Update Existing Event call here
+    let masterCourse = this.coursesMaster.find(c => c.id == course.id);
+    this.coursesMaster[this.coursesMaster.indexOf(masterCourse)] = <Course>JSON.parse(JSON.stringify(course));
+    let btn = document.getElementById(course.id + "UpdateButton");
+    if (!btn.className.includes("disabled")) {
+      btn.className += " disabled";
+    }
   }
 
   private saveCourse(course: Course): any {
@@ -44,6 +61,7 @@ export class CoursesPageComponent implements OnInit {
     console.log(course);
     // API Save New Event call here
     course.isDraft = false;
+    this.coursesMaster.push(<Course>JSON.parse(JSON.stringify(course)));
   }
 
   private switchSlide(course: Course): any {
@@ -56,25 +74,29 @@ export class CoursesPageComponent implements OnInit {
 
   private addCourse(): any {
     console.log('Add Course Called!');
-    var course: Course = new Course();
+    let course: Course = new Course();
     this.courses.push(course);
   }
 
-  private deleteCourse(course: Course): any {
+  private deleteCourse(): any {
     console.log('Delete Course Called!');
+    let course = this.courses.find(c => c.id == this.currentSlide);
     if (!course.isDraft) {
       console.log('API Call for delete called!');
       // API Call to delete this course
       // if (!successfulCall) { some error mssg + break; }
     }
     this.courses.splice(this.courses.indexOf(course), 1);
+    let courseMaster = this.coursesMaster.find(c => c.id == this.currentSlide);
+    this.coursesMaster.splice(this.coursesMaster.indexOf(courseMaster), 1);
   }
 
-  private addCourseOption(section: Array<CourseOption>): any {
+  private addCourseOption(course: Course, section: Array<CourseOption>): any {
     console.log('Add Course Option Called!');
     if (section.length < 26) {
       section.push(new CourseOption("ABCDEFGHIJKLMNOPQRSTUVWXYZ".charAt(section.length), Date.now().toString(), section.length));
     }
+    this.updateButtonCheck(course);
   }
 
   private removeCourseOption(course: Course, section: Array<CourseOption>, type: string): any {
@@ -99,6 +121,24 @@ export class CoursesPageComponent implements OnInit {
           break;
         default:
           break;
+      }
+    }
+    this.updateButtonCheck(course);
+  }
+
+  public updateButtonCheck(course: Course): any {
+    console.log('In Update Button Check!');
+    let masterCourse = this.coursesMaster.find(c => c.id == course.id);
+    let btn = document.getElementById(course.id + "UpdateButton");
+    if (btn && btn.className) {
+      if (JSON.stringify(masterCourse) !== JSON.stringify(course)) {
+        if (btn.className.includes("disabled")) {
+          btn.className = btn.className.replace(" disabled", "");
+        }
+      } else {
+        if (!btn.className.includes("disabled")) {
+          btn.className += " disabled";
+        }
       }
     }
   }
