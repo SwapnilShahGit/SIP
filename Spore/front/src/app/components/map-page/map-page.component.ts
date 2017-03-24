11d42a244c85f36e5d450c98c68d5037b28b6097
Pavlo Usoltsev <pavlo.usoltsev@soti.net>
2017-03-24 17:37:33 -0400

    #539 added some basic tools to display map, w-i-p

--- Spore/front/src/app/components/map-page/map-page.component.ts
+++ Spore/front/src/app/components/map-page/map-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MapResult } from '../../../meta/mapResult';
+declare var google;
 
 @Component({
   selector: 'map-page',
@@ -9,6 +10,31 @@ import { MapResult } from '../../../meta/mapResult';
 
 export class MapPageComponent {
 
+  private address: string = "";
+  private results: any = null;
+  private lat: number = 51.678418;
+  private lng: number = 7.809007;
+
   public ngOnInit() { }
+
+  private searchByAddress() {
+    let geocoder = new google.maps.Geocoder();
+    geocoder.geocode({'address': this.address },
+      function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          results.forEach(function(loc) {
+            //todo need the below values to be actually set so the map changes
+            console.log('*** Result found: ' + loc.formatted_address);
+            console.log('*** Lat: ' + loc.geometry.location.lat());
+            console.log('*** Lng: ' + loc.geometry.location.lng());
+          });
+        }
+      }
+    );
+  }
+
+  private geoLocateMe() {
+    console.log('Geo locate me called');
+  }
 
 }
