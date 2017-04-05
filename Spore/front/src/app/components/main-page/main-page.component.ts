dcf0b734488cdda5a6073525ec1dbb4aa43fcb19
Pranali Rathi <pranali.rathi@soti.net>
2017-04-05 16:14:28 -0400

    full functionality for tasks has been added [#486]

--- Spore/front/src/app/components/main-page/main-page.component.ts
+++ Spore/front/src/app/components/main-page/main-page.component.ts
@@ -44,6 +44,9 @@ export class MainPageComponent implements OnInit, OnDestroy {
 
   public ngOnInit() {
     this.user = this.databaseService.user;
+    this.user.subscribe((val) => {
+      console.log(val);
+    });
     console.log('_______ in main _______');
     if (this.cookieService.get('userID')) {
       this.userID = this.cookieService.get('userID');
