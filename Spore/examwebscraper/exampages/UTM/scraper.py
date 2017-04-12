beca95dd0a36fb8a7e902e64311f28bf43da471e
Swapnil Shah <swapnil.shah@mail.utoronto.ca>
2017-04-12 15:35:48 -0400

    add implementation for lecture section
    there is a new index in the JSON being returned that is named
    'Lecture Section'. This section handles support for different lecture
    sections that we can use for future implementation.
    
    [#565]

--- Spore/examwebscraper/exampages/UTM/scraper.py
+++ Spore/examwebscraper/exampages/UTM/scraper.py
@@ -2,33 +2,33 @@ from bs4 import BeautifulSoup
 import urllib2, json, io, time, os, datetime
 
 #figure out what exam this is for
-mydate = datetime.datetime.now()
-now = mydate.strftime("%B")
-filename = ""
-currentsemestercode = ""
-April = ["January", "February", "March", "April"]
-June = ["May", "June"]
-August = ["July", "August"]
-if now in April:
-    filename += "apr" + time.strftime("%y")
-    currentsemestercode = "1"
-elif now in June:
-    filename += "jun" + time.strftime("%y")
-    currentsemestercode = "5F"
-elif now in August:
-    filename += "aug" + time.strftime("%y")
-    currentsemestercode = "5S"
+myDate = datetime.datetime.now()
+now = myDate.strftime("%B")
+fileName = ""
+currentSemesterCode = ""
+aprilMonths = ["January", "February", "March", "April"]
+juneMonths = ["May", "June"]
+augustMonths = ["July", "August"]
+if now in aprilMonths:
+    fileName += "apr" + time.strftime("%y")
+    currentSemesterCode = "1"
+elif now in juneMonths:
+    fileName += "jun" + time.strftime("%y")
+    currentSemesterCode = "5F"
+elif now in augustMonths:
+    fileName += "aug" + time.strftime("%y")
+    currentSemesterCode = "5S"
 else:
-    filename += "dec" + time.strftime("%y")
-    currentsemestercode = "9"
+    fileName += "dec" + time.strftime("%y")
+    currentSemesterCode = "9"
 
 #create appropriate directory to store files
-if not os.path.exists(filename):
-    os.makedirs(filename)
+if not os.path.exists(fileName):
+    os.makedirs(fileName)
 
 #saving raw HTML into directory
 page_content = urllib2.urlopen("https://student.utm.utoronto.ca/examschedule/finalexams.php").read()
-with open(filename + '/rawpagesource.html', 'w') as f:
+with open(fileName + '/rawpagesource.html', 'w') as f:
   f.write(page_content)
 
 #start logic of the scraper
@@ -39,18 +39,25 @@ class_exams = []
 table = soup.find('table', attrs={'class', 'request_history schedule'})
 table_body = table.find('tbody')
 rows = table_body.find_all('tr')
+
 for row in rows:
     cols = row.find_all('td')
     cols = [ele.get_text() for ele in cols]
-    sep = ' '
+    sep = " "
+    temp = cols[0].split(sep, 1)
+    if len(temp) == 2:
+        temp = [cols[0].split(sep, 1)[1]]
+    else:
+        temp = ['LEC0101']
     cols[0] = cols[0].split(sep, 1)[0]
-    cols[0] = cols[0] + mydate.strftime("%Y") + currentsemestercode
+    cols[0] = cols[0] + myDate.strftime("%Y") + currentSemesterCode
+    cols += temp
     class_exams.append([ele.encode('ascii', 'ignore') for ele in cols if ele])
 
 #get names of columns
 table_head = table.find('thead')
 names = table_head.find_all('th')
-column_names = [ele.get_text() for ele in names]
+column_names = [ele.get_text() for ele in names] + ['Lecture Section']
 column_names = [ele.encode('ascii', 'ignore') for ele in column_names if ele]
 
 #match the column names with each individual row
@@ -65,5 +72,5 @@ for class_exam in class_exams:
     final_list.append(temp_list1)
 
 #save matched rows and columns into a JSON file inside newly created folder
-with io.open(filename + '/' + filename + ".json", 'w', encoding='utf-8') as f:
+with io.open(fileName + '/' + fileName + ".json", 'w', encoding='utf-8') as f:
   f.write(unicode(json.dumps(final_list)))
