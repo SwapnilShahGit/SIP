49238caa323c93697c24a81ca650439e1bfa8dc5
Darren Hobin <darren.hobin@soti.net>
2017-04-13 11:31:54 -0400

    Reformat code according to EditorConfig file
    This change performs automatic IDE code reformatting on a couple of files
    as specified by the EditorConfig file. This change is required for a
    future change in the same issue.
    
    [#584]

--- Spore/front/src/app/components/tasks/tasks.component.ts
+++ Spore/front/src/app/components/tasks/tasks.component.ts
@@ -1,7 +1,7 @@
-import { Component, Input, OnInit} from '@angular/core';
-import { User} from '../../../meta/user';
-import {TaskStore}from './TaskStore';
-import { DatabaseService } from '../../../meta/database.service';
+import {Component, Input, OnInit} from '@angular/core';
+import {User} from '../../../meta/user';
+import {TaskStore} from './TaskStore';
+import {DatabaseService} from '../../../meta/database.service';
 
 
 @Component({
@@ -10,10 +10,10 @@ import { DatabaseService } from '../../../meta/database.service';
   styleUrls: ['./tasks.component.scss']
 })
 
-export class TasksComponent implements OnInit{
-    private newTask = "";
-    private store = new TaskStore();
-    private items = this.store.items;
+export class TasksComponent implements OnInit {
+  private newTask = "";
+  private store = new TaskStore();
+  private items = this.store.items;
 
   @Input()
   public user: User;
@@ -21,34 +21,34 @@ export class TasksComponent implements OnInit{
   constructor(private databaseService: DatabaseService) {
   }
 
-  public ngOnInit(){
+  public ngOnInit() {
     this.items = this.user.tasks;
-  }  
+  }
 
-   public updateUser() {
-      let tempUser = this.user;
-      tempUser.tasks = this.items;
-      this.databaseService.updateUser(tempUser).then(response => {
-        if (response.error != '0') {
-          window.alert('Error occured during update API call: ' + response.data);
-        } else{
-          this.user = tempUser;
-        }
-      }); 
-   }
+  public updateUser() {
+    let tempUser = this.user;
+    tempUser.tasks = this.items;
+    this.databaseService.updateUser(tempUser).then(response => {
+      if (response.error != '0') {
+        window.alert('Error occured during update API call: ' + response.data);
+      } else {
+        this.user = tempUser;
+      }
+    });
+  }
 
   public addTask() {
     if (this.newTask === "") return;
     this.items.push({
-      text: this.newTask,  
-      done: false  
+      text: this.newTask,
+      done: false
     });
     this.updateUser();
     this.newTask = "";
   }
-    
+
   public removeTask(index: number) {
-    this.items.splice(index, 1); 
+    this.items.splice(index, 1);
     this.updateUser();
   }
 
