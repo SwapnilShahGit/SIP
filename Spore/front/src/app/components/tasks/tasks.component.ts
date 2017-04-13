847553266cfc2bedc2f9cee5a5c098bf92c821ee
Darren Hobin <darren.hobin@soti.net>
2017-04-13 11:28:10 -0400

    Rename taskStore to task-store
    This change makes imports case-consistent with the actual file names which
    is required for front end to be buildable in prod mode.
    
    [#584]

--- Spore/front/src/app/components/tasks/tasks.component.ts
+++ Spore/front/src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {User} from '../../../meta/user';
-import {TaskStore} from './TaskStore';
+import {TaskStore} from './task-store';
 import {DatabaseService} from '../../../meta/database.service';
 
 
