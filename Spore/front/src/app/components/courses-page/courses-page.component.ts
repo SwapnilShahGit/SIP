27fc84bb20d4c711953d50bfc27a88d68fa5ad59
Pavlo Usoltsev <pavlo.usoltsev@soti.net>
2017-04-26 10:50:18 -0400

    Connected Course APIs
    Can add, delete, save, update courses in courses tab
    
    [#534]

--- Spore/front/src/app/components/courses-page/courses-page.component.ts
+++ Spore/front/src/app/components/courses-page/courses-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../../../meta/course';
 import { CourseOption } from '../../../meta/courseOption';
+import { DatabaseService } from '../../../meta/database.service';
+import { CookieService } from 'angular2-cookie/core';
 
 @Component({
   selector: 'courses-page',
@@ -17,37 +19,96 @@ export class CoursesPageComponent implements OnInit {
   private searchDialogRequest: string = "";
   private mockedSearchResults: Array<Course> = [];
   private searchResultSelected: string = "";
+  private userID: string;
+
+  constructor (
+    private databaseService: DatabaseService,
+    private cookieService: CookieService
+  ) { }
 
   public ngOnInit() {
-    let lectures: Array<CourseOption> = [new CourseOption()];
-    let tutorials: Array<CourseOption> = [new CourseOption()];
-    let practicals: Array<CourseOption> = [new CourseOption(), new CourseOption()];
-    let officeHours: Array<CourseOption> = [new CourseOption()];
-    let course: Course = new Course(false, 'ABC123', 'Dr. Guy', 'No clue what this is...', null, true, "Exam will be held in IB 110 at 3:00pm until 5:00pm", lectures, tutorials, practicals, officeHours);
-    this.courses.push(course);
-    this.mockedSearchResults.push(course);
-    let courseMaster = <Course>JSON.parse(JSON.stringify(course));
-    this.coursesMaster.push(courseMaster);
-  }
+    // Mocked search results ---
+    this.mockedSearchResults.push(new Course(true, false, 'CSC108', 'M. Jackson',
+    'This course provides an Introduction to Computer Programming. By the end of this course, ' +
+    'you should be comfortable programming in Python, understand why good style is critical, ' +
+    'and be familiar with core computer science topics like algorithms and complexity.',
+    'ABC-DEF-123-GHGH-ZACH-123-ZACH-456', true, 'Held in IB110 from 2:00pm to 5:00pm.',
+    [new CourseOption("Tuesday", new Date(1970, 1, 1, 10, 30, 0, 0), new Date(1970, 1, 1, 12, 0, 0, 0)),
+    new CourseOption("Thursday", new Date(1970, 1, 1, 4, 0, 0, 0), new Date(1970, 1, 1, 6, 0, 0, 0))],
+    [new CourseOption("Friday", new Date(1970, 1, 1, 12, 0, 0, 0), new Date(1970, 1, 1, 13, 0, 0, 0))],
+    [],
+    '9:00am to 11:30am', 'DH6060', '#fabf8f'));
+    this.mockedSearchResults.push(new Course(true, false, 'CSC148', 'Dr. A. Rosenbloom',
+    'An investigation of many aspects of modern information security. Major topics cover: Techniques to identify and avoid common ' +
+    'software development flaws which leave software vulnerable to crackers. Utilizing modern operating systems security features to ' +
+    'deploy software in a protected environment. Common threats to networks and networked computers and tools to deal with them. ' +
+    'Cryptography and the role it plays in software development, systems security and network security.',
+    'ZACH-ZACH-ZACH-1234-1234-ZACH-123', true, 'Held in CC1080 from 9:00am to 11:00am.',
+    [new CourseOption("Wednesday", new Date(1970, 1, 1, 12, 30, 0, 0), new Date(1970, 1, 1, 15, 30, 0, 0))],
+    [],
+    [new CourseOption("Monday", new Date(1970, 1, 1, 4, 0, 0, 0), new Date(1970, 1, 1, 8, 0, 0, 0))],
+    '1:30pm to 4:00pm', 'OPH7010', '#cc99cc'));
+    // Mocked search results ---
 
-  private updateCourse(course: Course): any {
-    console.log('Course Update Called!');
-    console.log(course);
-    // API Update Existing Event call here
-    let masterCourse = this.coursesMaster.find(c => c.id == course.id);
-    this.coursesMaster[this.coursesMaster.indexOf(masterCourse)] = <Course>JSON.parse(JSON.stringify(course));
-    let btn = document.getElementById(course.id + "UpdateButton");
-    if (!btn.className.includes("disabled")) {
-      btn.className += " disabled";
+    if (this.cookieService.get('userID')) {
+      this.userID = this.cookieService.get('userID');
+      this.databaseService.getUserCourses(this.userID).then(response => {
+        if (response.error === 0) {
+          for (var i = 0; i < response.data.length; i++) {
+            this.cleanUpCourse(response.data[i]);
+          }
+
+          this.courses = response.data;
+          this.coursesMaster = JSON.parse(JSON.stringify(this.courses));
+        } else {
+          console.log('Error during course population: ' + response.data);
+        }
+      });
     }
   }
 
   private saveCourse(course: Course): any {
-    console.log('Course Save Called!');
-    console.log(course);
-    // API Save New Event call here
-    course.isDraft = false;
-    this.coursesMaster.push(<Course>JSON.parse(JSON.stringify(course)));
+    this.databaseService.addCourse(this.userID, course).then(response => {
+      if (response.error === 0) {
+        this.cleanUpCourse(response.data);
+        this.courses.splice(this.courses.indexOf(course), 1);
+        let courseMaster = this.coursesMaster.find(c => c.id == course.id);
+        this.coursesMaster.splice(this.coursesMaster.indexOf(courseMaster), 1);
+        this.courses.push(response.data);
+        this.coursesMaster.push(<Course>JSON.parse(JSON.stringify(response.data)));
+        setTimeout(function() {
+          let panel = document.getElementById(response.data.id + 'Toggle');
+          this.currentSlide = response.data.id;
+          if (panel) {
+            panel.click();
+          }
+        }, 50);
+      } else {
+        console.log('Error during course addition: ' + response.data);
+      }
+    });
+  }
+
+  private updateCourse(course: Course): any {
+    this.databaseService.updateCourse(course).then(response => {
+      if (response.error === 0) {
+        this.cleanUpCourse(response.data);
+        this.courses.splice(this.courses.indexOf(course), 1);
+        let courseMaster = this.coursesMaster.find(c => c.id == course.id);
+        this.coursesMaster.splice(this.coursesMaster.indexOf(courseMaster), 1);
+        this.courses.push(response.data);
+        this.coursesMaster.push(<Course>JSON.parse(JSON.stringify(response.data)));
+        setTimeout(function() {
+          let panel = document.getElementById(response.data.id + 'Toggle');
+          this.currentSlide = response.data.id;
+          if (panel) {
+            panel.click();
+          }
+        }, 50);
+      } else {
+        console.log('Error during course update: ' + response.data);
+      }
+    });
   }
 
   private switchSlide(course: Course): any {
@@ -59,13 +120,13 @@ export class CoursesPageComponent implements OnInit {
   }
 
   private addCourse(id?: string): any {
-    console.log('Add Course Called!');
     let course: Course;
     if (id) {
       course = this.mockedSearchResults.find(c => c.id == id);
     } else {
       course = new Course();
     }
+
     this.courses.push(course);
     setTimeout(function() {
       let panel = document.getElementById(course.id + 'Toggle');
@@ -76,24 +137,31 @@ export class CoursesPageComponent implements OnInit {
           courseField.focus();
         }
       }
-    }, 50)
+    }, 50);
   }
 
   private deleteCourse(): any {
-    console.log('Delete Course Called!');
     let course = this.courses.find(c => c.id == this.currentSlide);
-    if (!course.isDraft) {
-      console.log('API Call for delete called!');
-      // API Call to delete this course
-      // if (!successfulCall) { some error mssg + break; }
+    if (!course.isDraft || course.is_parse) {
+      this.databaseService.deleteCourse(this.userID, course).then(response => {
+        if (response.status === 200) {
+          this.removeFromCourseList(course);
+        } else {
+          console.log('Error during course delete, status: ' + response.status + '.');
+        }
+      });
+    } else {
+      this.removeFromCourseList(course);
     }
+  }
+
+  private removeFromCourseList(course: Course): any {
     this.courses.splice(this.courses.indexOf(course), 1);
     let courseMaster = this.coursesMaster.find(c => c.id == this.currentSlide);
     this.coursesMaster.splice(this.coursesMaster.indexOf(courseMaster), 1);
   }
 
   private addCourseOption(course: Course, section: Array<CourseOption>): any {
-    console.log('Add Course Option Called!');
     if (section.length < 5) {
       section.push(new CourseOption());
     }
@@ -101,13 +169,11 @@ export class CoursesPageComponent implements OnInit {
   }
 
   private removeCourseOption(course: Course, section: Array<CourseOption>, type: string): any {
-    console.log('Remove Course Option Called!');
     section.pop();
     this.updateButtonCheck(course);
   }
 
   private updateButtonCheck(course: Course): any {
-    console.log('In Update Button Check!');
     let masterCourse = this.coursesMaster.find(c => c.id == course.id);
     let btn = document.getElementById(course.id + "UpdateButton");
     if (btn && btn.className) {
@@ -131,7 +197,47 @@ export class CoursesPageComponent implements OnInit {
   }
 
   private toggleSwitch(course: Course): any {
-    course.examNotifications = !course.examNotifications;
+    course.exams = !course.exams;
     this.updateButtonCheck(course);
+  }
+
+  private cleanUpCourse(course: any) {
+    course.id = course._id;
+    if (course.lectures) {
+      for (var j = 0; j < course.lectures.length; j++) {
+        course.lectures[j].startTime = new Date(course.lectures[j].startTime);
+        course.lectures[j].endTime = new Date(course.lectures[j].endTime);
+      }
+    } else {
+      course.lectures = Array<CourseOption>();
+    }
+
+    if (course.tutorials) {
+      for (j = 0; j < course.tutorials.length; j++) {
+        course.tutorials[j].startTime = new Date(course.tutorials[j].startTime);
+        course.tutorials[j].endTime = new Date(course.tutorials[j].endTime);
+      }
+    } else {
+      course.tutorials = Array<CourseOption>();
+    }
+
+    if (course.practicals) {
+      for (j = 0; j < course.practicals.length; j++) {
+        course.practicals[j].startTime = new Date(course.practicals[j].startTime);
+        course.practicals[j].endTime = new Date(course.practicals[j].endTime);
+      }
+    } else {
+      course.practicals = Array<CourseOption>();
+    }
+
+    if (course.colour) {
+      course.colour = course.colour.toLowerCase();
+    } else {
+      course.colour = '#ffcc66';
+    }
+
+    if (course.office_hours && course.office_location) {
+      course.officeHoursInfo = 'Held in ' + course.office_location + ' from ' + course.office_hours + '.';
+    }
   }
 }
