304761522180ebc7135fafdecb9daf3dbf2e8038
Adam Natale <adam.natale@soti.net>
2017-04-26 22:07:22 -0400

    Update course object to meet backend requirements
    Lectures tutorials and practicals followed incorrect format
    
    [#664]

--- Spore/front/src/meta/courseOption.ts
+++ Spore/front/src/meta/courseOption.ts
@@ -1,18 +1,15 @@
 
 export class CourseOption implements ICourseOption {
-    public day: string;
-    public startTime: Date;
-    public endTime: Date;
+    public times: {day: string, start: Date, end: Date}[] = [];
 
     constructor(day?: string, startTime?: Date, endTime?: Date) {
-      this.day = day || "Monday";
-      this.startTime = startTime || new Date(1970, 0, 1, new Date().getHours(), 0, 0, 0);
-      this.endTime = endTime || new Date(1970, 0, 1, new Date().getHours(), 0, 0, 0);
+      let newDay = day || "Monday";
+      let newStartTime = startTime || new Date(1970, 0, 1, new Date().getHours(), 0, 0, 0);
+      let newEndTime = endTime || new Date(1970, 0, 1, new Date().getHours(), 0, 0, 0);
+      this.times.push({day: newDay, start: newStartTime, end: newEndTime});
     }
 }
 
 export interface ICourseOption {
-    day: string,
-    startTime: Date,
-    endTime: Date
+
 }
